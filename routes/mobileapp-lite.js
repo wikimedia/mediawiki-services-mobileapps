170808a8716b0ae0357b0a24bc0df4b3be237a24
Dmitry Brant <dbrant@wikimedia.org>
2015-05-25 11:14:53 +0200

    Expanded the Lite service to break down sections into paragraphs.

--- routes/mobileapp-lite.js
+++ routes/mobileapp-lite.js
@@ -56,17 +56,87 @@ function rmSelectorAll(doc, selector) {
     }
 }
 
+function structureThumbnail(thumbDiv) {
+    var thumb = {};
+    var thumbimg = thumbDiv.querySelector("img");
+    if (thumbimg) {
+        thumb.src = thumbimg.getAttribute("src");
+    }
+    var videoDiv = thumbDiv.querySelector("div.PopUpMediaTransform");
+    if (videoDiv && thumbimg) {
+        thumb.type = "video";
+        thumb.name = thumbimg.alt;
+    } else {
+        thumb.type = "image";
+        var alink = thumbDiv.querySelector("a");
+        if (alink) {
+            thumb.name = alink.href;
+        }
+    }
+    var caption = thumbDiv.querySelector("div.thumbcaption");
+    if (caption) {
+        thumb.caption = caption.innerHTML;
+    }
+    return thumb;
+}
+
 /**
  * Nuke stuff from the DOM we don't want.
  */
-function runDomTransforms(text) {
-    var doc = domino.createDocument(text);
+function runDomTransforms(section) {
+    var doc = domino.createDocument(section.text);
     rmSelectorAll(doc, 'div.noprint');
     rmSelectorAll(doc, 'div.infobox');
     rmSelectorAll(doc, 'div.hatnote');
     rmSelectorAll(doc, 'div.metadata');
     rmSelectorAll(doc, 'table'); // TODO: later we may want to transform some of the tables into a JSON structure
-    return doc.body.innerHTML;
+
+
+    // and break it down into items...
+    section.items = [];
+    var itemIndex = 0;
+    var thumbnails, tid, thumb;
+
+    var ps = doc.querySelectorAll("p") || [];
+    for (var pid = 0; pid < ps.length; pid++) {
+        var p = ps[pid];
+
+        if (p.innerHTML.length < 4) {
+            continue;
+        }
+
+        section.items[itemIndex] = {};
+        section.items[itemIndex].type = "p";
+        section.items[itemIndex].text = p.innerHTML;
+        itemIndex++;
+
+        // find all images in this paragraph, and append them as section items
+        thumbnails = p.querySelectorAll("div.thumb") || [];
+        for (tid = 0; tid < thumbnails.length; tid++) {
+            thumb = thumbnails[tid];
+            section.items[itemIndex] = structureThumbnail(thumb);
+            itemIndex++;
+        }
+
+        // remove other inline images from this paragraph
+        rmSelectorAll(p, 'img');
+        // and remove this paragraph from the DOM
+        p.parentNode.removeChild(p);
+    }
+
+
+    // find all images in this section (outside of paragraphs), and append them as section items
+    thumbnails = doc.querySelectorAll("div.thumb") || [];
+    for (tid = 0; tid < thumbnails.length; tid++) {
+        thumb = thumbnails[tid];
+        section.items[itemIndex] = structureThumbnail(thumb);
+        itemIndex++;
+    }
+
+
+
+
+    delete section.text;
 }
 
 /**
@@ -79,11 +149,11 @@ router.get('/:title', function (req, res) {
         "action": "mobileview",
         "format": "json",
         "page": req.params.title,
-        "prop": "text|sections",
+        "prop": "text|sections|languagecount|thumb|image|id|revision|description|lastmodified|normalizedtitle|displaytitle|protection|editable",
         "sections": "all",
         "sectionprop": "toclevel|line|anchor",
-        "noheadings": true,
-        "noimages": true
+        "thumbsize": "640",
+        "noheadings": true
     };
 
     return apiGet(req.params.domain, apiParams)
@@ -104,7 +174,8 @@ router.get('/:title', function (req, res) {
             var sections = apiRes.body.mobileview.sections;
             for (var idx = 0; idx < sections.length; idx++) {
                 var section = sections[idx];
-                section.text = runDomTransforms(section.text);
+                // run DOM transforms on the section...
+                runDomTransforms(section);                
             }
 
             res.status(200).type('json').end(JSON.stringify(apiRes.body.mobileview));
