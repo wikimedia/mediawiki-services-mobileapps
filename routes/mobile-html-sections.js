ccdbb865c4d7ef90914971338f5dd7fe19ee0c0e
bearND <bsitzmann@wikimedia.org>
2015-08-05 16:33:59 -0600

    Hygiene: remove code duplication
    Created new modules in lib folder for mwapi, transforms, gallery
    so that common code in mobile-html and mobile-html-sections is found
    in the lib modules.
    
    Change-Id: I08d98e22f44fa7e9f65de1649d5ee923b8e112bb

--- routes/mobile-html-sections.js
+++ routes/mobile-html-sections.js
@@ -13,8 +13,10 @@
 
 var BBPromise = require('bluebird');
 var preq = require('preq');
-var domino = require('domino');
 var sUtil = require('../lib/util');
+var transforms = require('../lib/transforms');
+var mwapi = require('../lib/mwapi');
+var gallery = require('../lib/gallery');
 
 // shortcut
 var HTTPError = sUtil.HTTPError;
@@ -30,331 +32,32 @@ var router = sUtil.router();
  */
 var app;
 
-// gallery constants:
-var MAX_ITEM_COUNT = 256;
-var MIN_IMAGE_SIZE = 64;
-var MAX_IMAGE_WIDTH = 1280;
-
-
 function dbg(name, obj) {
     //console.log("DEBUG: " + name + ": " + JSON.stringify(obj, null, 2));
     app.logger.log('debug', name + ": " + JSON.stringify(obj));
 }
 
-/**
- * A helper function that obtains the HTML from the MW API and
- * loads it into a domino DOM document instance.
- *
- * @param {String} domain the domain to contact
- * @param {Object} params an Object with all the query parameters for the MW API
- * @return {Promise} a promise resolving as the HTML element object
- */
-function apiGet(domain, params) {
-    // get the page from the MW API
-    return preq.get({
-        uri: 'http://' + domain + '/w/api.php',
-        query: params
-    });
-}
-
-function rmSelectorAll(doc, selector) {
-    var ps = doc.querySelectorAll(selector) || [];
-    for (var idx = 0; idx < ps.length; idx++) {
-        var node = ps[idx];
-        node.parentNode.removeChild(node);
-    }
-}
-
-function rmBracketSpans(doc) {
-    var ps = doc.querySelectorAll('span:not([class],[style],[id])') || [];
-    for (var idx = 0; idx < ps.length; idx++) {
-        var node = ps[idx];
-        if (node.innerHTML === '[') {
-            var leftBracket = doc.createTextNode('[');
-            node.parentNode.replaceChild(leftBracket, node);
-        } else if (node.innerHTML === ']') {
-            var rightBracket = doc.createTextNode(']');
-            node.parentNode.replaceChild(rightBracket, node);
-        }
-    }
-}
-
-function rmAttributeAll(doc, selector, attribute) {
-    var ps = doc.querySelectorAll(selector) || [];
-    for (var idx = 0; idx < ps.length; idx++) {
-        var node = ps[idx];
-        node.removeAttribute(attribute);
-    }
-}
-
-function moveFirstParagraphUpInLeadSection(text) {
-    var doc = domino.createDocument(text);
-    // TODO: mhurd: feel free to add your lead section magic here
-    return doc.body.innerHTML;
-}
-
-/**
- * Nukes stuff from the DOM we don't want.
- */
-function runDomTransforms(text, sectionIndex) {
-    var doc = domino.createDocument(text);
-
-    var rmSelectors = [
-        'div.noprint',
-        'div.infobox',
-        'div.metadata',
-        'table.navbox',
-        'div.magnify',
-        'span[style*="display:none"]',             // Remove <span style=\"display:none;\">&nbsp;</span>
-        'span.Z3988'                               // Remove <span class=\"Z3988\"></span>
-    ];
-    if (sectionIndex === 0) {
-        rmSelectors.push('div.hatnote');
-    }
-    rmSelectorAll(doc, rmSelectors.join(', '));    // Do single call to rmSelectorAll.
-
-    rmAttributeAll(doc, 'a', 'rel');
-    rmAttributeAll(doc, 'a,span', 'title');
-    rmAttributeAll(doc, 'img', 'alt');
-
-    rmBracketSpans(doc);
-
-    // TODO: mhurd: add more references to functions where you do more transforms here
-
-    return doc.body.innerHTML;
-}
-
-function checkApiResponse(response) {
-    // check if the query failed
-    if (response.status > 299) {
-        // there was an error in the MW API, propagate that
-        throw new HTTPError({
-            status: response.status,
-            type: 'api_error',
-            title: 'MW API error',
-            detail: response.body
-        });
-    }
-}
-
-function checkForQueryPagesIn(response) {
-    if (!response.body.query || !response.body.query.pages) {
-        // we did not get our expected query.pages from the MW API, propagate that
-        app.logger.log('error', 'no query.pages in response: ' + JSON.stringify(response, null, 2));
-        throw new HTTPError({
-            status: response.status,
-            type: 'api_error',
-            title: 'no query.pages in response',
-            detail: response.body
-        });
-    }
-}
-
 /** Returns a promise to retrieve the page content from MW API mobileview */
 function pageContentPromise(domain, title) {
-    return apiGet(domain, {
-        "action": "mobileview",
-        "format": "json",
-        "page": title,
-        "prop": "text|sections|thumb|image|id|revision|description|lastmodified|normalizedtitle|displaytitle|protection|editable",
-        "sections": "all",
-        "sectionprop": "toclevel|line|anchor",
-        "noheadings": true
-    }).then(function (response) {
-        checkApiResponse(response);
+    return mwapi.getAllSections(domain, title)
+    .then(function (response) {
+        mwapi.checkApiResponse(response);
 
         // transform all sections
         var sections = response.body.mobileview.sections;
         for (var idx = 0; idx < sections.length; idx++) {
             var section = sections[idx];
-            section.text = runDomTransforms(section.text, idx);
+            section.text = transforms.runDomTransforms(section.text, idx);
         }
 
         if (!response.body.mobileview.mainpage) {
             // don't do anything if this is the main page, since many wikis
             // arrange the main page in a series of tables.
             // TODO: should we also exclude file and other special pages?
-            sections[0].text = moveFirstParagraphUpInLeadSection(sections[0].text);
+            sections[0].text = transforms.moveFirstParagraphUpInLeadSection(sections[0].text);
         }
 
         return response.body.mobileview;
-    });
-}
-
-// in the case of video, look for a list of transcodings, so that we might
-// find a WebM version, which is playable in Android.
-function getTranscodedVideoUrl(objinfo) {
-    var derivativesArr, derivative, url, key;
-    if (objinfo.derivatives) {
-        derivativesArr = objinfo.derivatives;
-        for (key in derivativesArr) {
-            if (derivativesArr.hasOwnProperty(key)) {
-                derivative = derivativesArr[key];
-                if (derivative.type && derivative.type.indexOf("webm") > -1) {
-                    // that's the one!
-                    url = derivative.src;
-                    // Note: currently picks the last one
-                    // TODO: in the future we could have an extra URL that provides a size parameter for images and videos
-                }
-            }
-        }
-    }
-    return url;
-}
-
-function getExtMetadata(extmetadata) {
-    var ext = {};
-    for (var key in extmetadata) {
-        if (extmetadata.hasOwnProperty(key)) {
-            var value = extmetadata[key].value;
-            if (typeof value === "string") {
-                ext[key] = value.trim();
-            }
-        }
-    }
-    return ext;
-}
-
-function handleGalleryItems(item) {
-    var obj, url;
-
-    if (item.imageinfo) {
-        obj = item.imageinfo[0];
-    } else if (item.videoinfo) {
-        obj = item.videoinfo[0];
-        url = getTranscodedVideoUrl(obj);
-    }
-    if (!url) {
-        url = obj.url;
-    }
-    return {
-        title: item.title,
-        url: url,
-        thumbUrl: obj.thumbUrl,
-        mimeType: obj.mimeType,
-        width: obj.width,
-        height: obj.height,
-        ext: getExtMetadata(obj.extmetadata)
-    };
-}
-
-function onGalleryItemsResponse(response) {
-    var items, key;
-    var output = [];
-
-    checkApiResponse(response);
-    checkForQueryPagesIn(response);
-
-    items = response.body.query.pages;
-    for (key in items) {
-        if (items.hasOwnProperty(key)) {
-            output.push(handleGalleryItems(items[key]));
-        }
-    }
-
-    return output;
-}
-
-/** Returns a promise to retrieve one or more gallery items. */
-function galleryItemsPromise(domain, titles, params) {
-    Object.assign(params, {
-        "action": "query",
-        "format": "json",
-        "titles": titles,
-        "continue": ""
-    });
-
-    return apiGet(domain, params)
-        .then(function (response) {
-            return onGalleryItemsResponse(response);
-        });
-}
-
-function onGalleryCollectionsResponse(response, domain) {
-    var detailsPromises = [], videos = [], images = [];
-    var isVideo;
-
-    checkApiResponse(response);
-    if (!response.body.query || !response.body.query.pages) {
-        return [];
-    }
-
-    // iterate over all items
-    var items = response.body.query.pages;
-    for (var key in items) {
-        if (items.hasOwnProperty(key)) {
-            var item = items[key];
-
-            // remove the ones that are too small or are of the wrong type
-            var imageinfo = item.imageinfo[0];  // TODO: why this is an array?
-
-            // Reject gallery items if they're too small.
-            // Also reject SVG and PNG items by default, because they're likely to be
-            // logos and/or presentational images.
-            if (imageinfo.width < MIN_IMAGE_SIZE
-                || imageinfo.height < MIN_IMAGE_SIZE
-                || imageinfo.mime.indexOf("svg") > -1
-                || imageinfo.mime.indexOf("png") > -1
-            ) {
-                delete items[key];
-            } else {
-                delete item.ns;
-                delete item.imagerepository; // we probably don't care where the repo is
-                delete imageinfo.size;
-                // TODO instead of deleting properties we probably want to just add well-known properties
-
-                isVideo = imageinfo.mime.indexOf("ogg") > -1 || imageinfo.mime.indexOf("video") > -1;
-
-                // request details individually, to keep the order
-                //detailsPromises.push(galleryItemsPromise(domain, item.title, isVideo));
-
-                if (isVideo) {
-                    videos.push(item.title);
-                } else {
-                    images.push(item.title);
-                }
-            }
-        }
-    }
-
-    // one more request for all the videos
-    if (videos.length > 0) {
-        detailsPromises.videos = galleryItemsPromise(domain, videos.join('|'), {
-            "prop": "videoinfo",
-            "viprop": "url|dimensions|mime|extmetadata|derivatives",
-            "viurlwidth": MAX_IMAGE_WIDTH,
-        });
-    }
-
-    // another one request for all the images
-    if (images.length > 0) {
-        detailsPromises.images = galleryItemsPromise(domain, images.join('|'), {
-            "prop": "imageinfo",
-            "iiprop": "url|dimensions|mime|extmetadata",
-            "iiurlwidth": MAX_IMAGE_WIDTH
-        });
-    }
-
-    return detailsPromises;
-}
-
-/** Gets the gallery content from MW API */
-function galleryCollectionPromise(domain, title) {
-    return apiGet(domain, {
-        "action": "query",
-        "format": "json",
-        "titles": title,
-        "continue": "",
-        "prop": "imageinfo",
-        "iiprop": "dimensions|mime",
-        "generator": "images",
-        "gimlimit": MAX_ITEM_COUNT
-    }).then(function (response) {
-        var detailsPromises = onGalleryCollectionsResponse(response, domain);
-        return BBPromise.props({
-            videos: detailsPromises.videos,
-            images: detailsPromises.images
-        });
     });
 }
 
@@ -365,7 +68,7 @@ function galleryCollectionPromise(domain, title) {
 router.get('/mobile-html-sections/:title', function (req, res) {
     BBPromise.props({
         page: pageContentPromise(req.params.domain, req.params.title),
-        media: galleryCollectionPromise(req.params.domain, req.params.title)
+        media: gallery.collectionPromise(req.logger, req.params.domain, req.params.title)
     }).then(function (response) {
         res.status(200).type('json').end(JSON.stringify(response));
     });
--- routes/mobile-html.js
+++ routes/mobile-html.js
@@ -20,9 +20,9 @@ var preq = require('preq');
 var domino = require('domino');
 var sUtil = require('../lib/util');
 var util = require('util');
-
-// shortcut
-var HTTPError = sUtil.HTTPError;
+var transforms = require('../lib/transforms');
+var mwapi = require('../lib/mwapi');
+var gallery = require('../lib/gallery');
 
 
 /**
@@ -37,70 +37,12 @@ var app;
 
 var DEBUG = true;
 
-// gallery constants:
-var MAX_ITEM_COUNT = 256;
-var MIN_IMAGE_SIZE = 64;
-var MAX_IMAGE_WIDTH = 1280;
-
-
 function dbg(name, obj) {
     if (DEBUG) {
         console.log("DEBUG: " + name + ": " + util.inspect(obj));
         //console.log("DEBUG: " + name + ": " + JSON.stringify(obj, null, 2));
         //app.logger.log('debug', name + ": " + JSON.stringify(obj));
     }
-}
-
-/**
- * A helper function that obtains the HTML from the MW API and
- * loads it into a domino DOM document instance.
- *
- * @param {String} domain the domain to contact
- * @param {Object} params an Object with all the query parameters for the MW API
- * @return {Promise} a promise resolving as the HTML element object
- */
-function apiGet(domain, params) {
-    // get the page from the MW API
-    return preq.get({
-        uri: 'http://' + domain + '/w/api.php',
-        query: params
-    });
-}
-
-function rmSelectorAll(doc, selector) {
-    var ps = doc.querySelectorAll(selector) || [];
-    for (var idx = 0; idx < ps.length; idx++) {
-        var node = ps[idx];
-        node.parentNode.removeChild(node);
-    }
-}
-
-function rmBracketSpans(doc) {
-    var ps = doc.querySelectorAll('span:not([class],[style],[id])') || [];
-    for (var idx = 0; idx < ps.length; idx++) {
-        var node = ps[idx];
-        if (node.innerHTML === '[') {
-            var leftBracket = doc.createTextNode('[');
-            node.parentNode.replaceChild(leftBracket, node);
-        } else if (node.innerHTML === ']') {
-            var rightBracket = doc.createTextNode(']');
-            node.parentNode.replaceChild(rightBracket, node);
-        }
-    }
-}
-
-function rmAttributeAll(doc, selector, attribute) {
-    var ps = doc.querySelectorAll(selector) || [];
-    for (var idx = 0; idx < ps.length; idx++) {
-        var node = ps[idx];
-        node.removeAttribute(attribute);
-    }
-}
-
-function moveFirstParagraphUpInLeadSection(text) {
-    var doc = domino.createDocument(text);
-    // TODO: mhurd: feel free to add your lead section magic here
-    return doc.body.innerHTML;
 }
 
 /**
@@ -131,6 +73,7 @@ function buildSectionHeading(section) {
     return heading.outerHTML;
 }
 
+/** For Parsoid payload */
 function rmTemplates(doc, names) {
     var nodes = doc.querySelectorAll('[typeof~=mw:Transclusion]');
     var dataMW;
@@ -160,77 +103,6 @@ function rmTemplates(doc, names) {
                 node.parentNode.removeChild(node);
             }
         }
-    }
-}
-
-
-/**
- * Nukes stuff from the DOM we don't want.
- */
-function runDomTransforms(doc) {
-
-    // Demo: remove content produced by specific templates
-    //rmTemplates(doc, {
-    //    reflist: true
-    //});
-
-    var rmSelectors = [
-        'div.noprint',
-        'div.infobox', // ??
-        'div.metadata',
-        'table.navbox', // template
-        'div.magnify',
-        'span[style*="display:none"]',             // Remove <span style=\"display:none;\">&nbsp;</span>
-        'span.Z3988',                              // Remove <span class=\"Z3988\"></span>
-        'div.hatnote',
-
-        // Demo: remove specific extensions, in this case citations
-        //'[typeof~=mw:Extension/ref]'
-    ];
-
-    rmSelectorAll(doc, rmSelectors.join(', '));    // Do single call to rmSelectorAll.
-
-    rmAttributeAll(doc, 'html', 'prefix');
-    rmAttributeAll(doc, 'a', 'rel');
-    rmAttributeAll(doc, 'a,span', 'title');
-    rmAttributeAll(doc, 'img', 'alt');
-    rmAttributeAll(doc, '*', 'data-mw'); // Remove template parameters etc
-    rmAttributeAll(doc, '*[id^=mw]', 'id'); // Remove Parsoid-assigned IDs
-    rmAttributeAll(doc, '*', 'about'); // RDFa about
-
-    rmBracketSpans(doc);
-
-
-    // TODO: mhurd: add more references to functions where you do more transforms here
-    //content = transformer.transform("section", content);
-    //content = transformer.transform("hideTables", content);
-    //content = transformer.transform("hideIPA", content);
-    //content = transformer.transform("hideRefs", content );
-}
-
-function checkApiResponse(response) {
-    // check if the query failed
-    if (response.status > 299) {
-        // there was an error in the MW API, propagate that
-        throw new HTTPError({
-            status: response.status,
-            type: 'api_error',
-            title: 'MW API error',
-            detail: response.body
-        });
-    }
-}
-
-function checkForQueryPagesInResponse(logger, response) {
-    if (!response.body.query || !response.body.query.pages) {
-        // we did not get our expected query.pages from the MW API, propagate that
-        logger.log('error', 'no query.pages in response: ' + JSON.stringify(response, null, 2));
-        throw new HTTPError({
-            status: response.status,
-            type: 'api_error',
-            title: 'no query.pages in response',
-            detail: response.body
-        });
     }
 }
 
@@ -296,7 +168,7 @@ function compileHtml(html, meta1, meta2) {
     doc.head.innerHTML = '';
 
     // Strip some content
-    runDomTransforms(doc);
+    transforms.runDomTransforms(doc);
 
     addToHtmlHead(doc);
     var body = doc.body;
@@ -361,16 +233,9 @@ function buildPageContentJSON(orig) {
 
 /** Returns a promise to retrieve the page content from MW API mobileview */
 function pageContentPromise(domain, title) {
-    return apiGet(domain, {
-        "action": "mobileview",
-        "format": "json",
-        "page": title,
-        "prop": "text|sections|thumb|image|id|revision|description|lastmodified|normalizedtitle|displaytitle|protection|editable",
-        "sections": "all",
-        "sectionprop": "toclevel|line|anchor",
-        "noheadings": true
-    }).then(function (response) {
-        checkApiResponse(response);
+    return mwapi.getAllSections(domain, title)
+    .then(function (response) {
+        mwapi.checkApiResponse(response);
 
         //// transform all sections
         //var sections = response.body.mobileview.sections;
@@ -384,193 +249,12 @@ function pageContentPromise(domain, title) {
         //    // don't do anything if this is the main page, since many wikis
         //    // arrange the main page in a series of tables.
         //    // TODO: should we also exclude file and other special pages?
-        //    sections[0].text = moveFirstParagraphUpInLeadSection(sections[0].text);
+        //    sections[0].text = transforms.moveFirstParagraphUpInLeadSection(sections[0].text);
         //}
 
         return {
             json: buildPageContentJSON(response.body.mobileview)
         };
-    });
-}
-
-// in the case of video, look for a list of transcodings, so that we might
-// find a WebM version, which is playable in Android.
-function getTranscodedVideoUrl(objinfo) {
-    var derivativesArr, derivative, url, key;
-    if (objinfo.derivatives) {
-        derivativesArr = objinfo.derivatives;
-        for (key in derivativesArr) {
-            if (derivativesArr.hasOwnProperty(key)) {
-                derivative = derivativesArr[key];
-                if (derivative.type && derivative.type.indexOf("webm") > -1) {
-                    // that's the one!
-                    url = derivative.src;
-                    // Note: currently picks the last one
-                    // TODO: in the future we could have an extra URL that provides a size parameter for images and videos
-                }
-            }
-        }
-    }
-    return url;
-}
-
-function getExtMetadata(extmetadata) {
-    var ext = {};
-    for (var key in extmetadata) {
-        if (extmetadata.hasOwnProperty(key)) {
-            var value = extmetadata[key].value;
-            if (typeof value === "string") {
-                ext[key] = value.trim();
-            }
-        }
-    }
-    return ext;
-}
-
-function handleGalleryItems(item) {
-    var obj, url;
-
-    if (item.imageinfo) {
-        obj = item.imageinfo[0];
-    } else if (item.videoinfo) {
-        obj = item.videoinfo[0];
-        url = getTranscodedVideoUrl(obj);
-    }
-    if (!url) {
-        url = obj.url;
-    }
-    return {
-        title: item.title,
-        url: url,
-        thumbUrl: obj.thumbUrl,
-        mimeType: obj.mimeType,
-        width: obj.width,
-        height: obj.height,
-        ext: getExtMetadata(obj.extmetadata)
-    };
-}
-
-function onGalleryItemsResponse(logger, response) {
-    var items, key;
-    var output = [];
-
-    checkApiResponse(response);
-    checkForQueryPagesInResponse(logger, response);
-
-    items = response.body.query.pages;
-    for (key in items) {
-        if (items.hasOwnProperty(key)) {
-            output.push(handleGalleryItems(items[key]));
-        }
-    }
-
-    return output;
-}
-
-/** Returns a promise to retrieve one or more gallery items. */
-function galleryItemsPromise(logger, domain, titles, params) {
-    Object.assign(params, {
-        "action": "query",
-        "format": "json",
-        "titles": titles,
-        "continue": ""
-    });
-
-    return apiGet(domain, params)
-        .then(function (response) {
-            return onGalleryItemsResponse(logger, response);
-        });
-}
-
-function onGalleryCollectionsResponse(logger, response, domain) {
-    var detailsPromises = [], videos = [], images = [];
-    var isVideo;
-
-    checkApiResponse(response);
-    if (!response.body.query || !response.body.query.pages) {
-        return [];
-    }
-
-    // iterate over all items
-    var items = response.body.query.pages;
-    for (var key in items) {
-        if (items.hasOwnProperty(key)) {
-            var item = items[key];
-
-            if (!item.imageinfo || !(item.imageinfo instanceof Array)) {
-                continue;
-            }
-
-            // remove the ones that are too small or are of the wrong type
-            var imageinfo = item.imageinfo[0];  // TODO: why this is an array?
-
-            // Reject gallery items if they're too small.
-            // Also reject SVG and PNG items by default, because they're likely to be
-            // logos and/or presentational images.
-            if (imageinfo.width < MIN_IMAGE_SIZE
-                || imageinfo.height < MIN_IMAGE_SIZE
-                || imageinfo.mime.indexOf("svg") > -1
-                || imageinfo.mime.indexOf("png") > -1
-            ) {
-                delete items[key];
-            } else {
-                delete item.ns;
-                delete item.imagerepository; // we probably don't care where the repo is
-                delete imageinfo.size;
-                // TODO instead of deleting properties we probably want to just add well-known properties
-
-                isVideo = imageinfo.mime.indexOf("ogg") > -1 || imageinfo.mime.indexOf("video") > -1;
-
-                // request details individually, to keep the order
-                //detailsPromises.push(galleryItemsPromise(domain, item.title, isVideo));
-
-                if (isVideo) {
-                    videos.push(item.title);
-                } else {
-                    images.push(item.title);
-                }
-            }
-        }
-    }
-
-    // one more request for all videos
-    if (videos.length > 0) {
-        detailsPromises.videos = galleryItemsPromise(logger, domain, videos.join('|'), {
-            "prop": "videoinfo",
-            "viprop": "url|dimensions|mime|extmetadata|derivatives",
-            "viurlwidth": MAX_IMAGE_WIDTH
-        });
-    }
-
-    // another request for all images
-    if (images.length > 0) {
-        detailsPromises.images = galleryItemsPromise(logger, domain, images.join('|'), {
-            "prop": "imageinfo",
-            "iiprop": "url|dimensions|mime|extmetadata",
-            "iiurlwidth": MAX_IMAGE_WIDTH
-        });
-    }
-
-    return detailsPromises;
-}
-
-/** Gets the gallery content from MW API */
-function galleryCollectionPromise(logger, domain, title) {
-    return apiGet(domain, {
-        "action": "query",
-        "format": "json",
-        "titles": title,
-        "continue": "",
-        "prop": "imageinfo",
-        "iiprop": "dimensions|mime",
-        "generator": "images",
-        "gimlimit": MAX_ITEM_COUNT
-    }).then(function (response) {
-        var detailsPromises = onGalleryCollectionsResponse(logger, response, domain);
-        return BBPromise.props({
-            videos: detailsPromises.videos,
-            images: detailsPromises.images
-        });
     });
 }
 
@@ -586,7 +270,7 @@ router.get('/mobile-html/:title', function (req, res) {
             + '/v1/page/html/' + encodeURIComponent(req.params.title),
         }),
         page: pageContentPromise(req.params.domain, req.params.title),
-        media: galleryCollectionPromise(req.logger, req.params.domain, req.params.title)
+        media: gallery.collectionPromise(req.logger, req.params.domain, req.params.title)
     }).then(function (response) {
         var html = compileHtml(response.html.body, response.page.json, response.media);
         res.status(200).type('html').end(html);
