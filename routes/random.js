365adeba0e090e0010d5bec4bee5b92e63ea51d4
montehurd <montehurd@gmail.com>
2016-05-05 14:41:28 -0600

    Add "random" feed endpoint
    http://localhost:6927/en.wikipedia.org/v1/page/random/summary
    
    Bug: T135209
    Change-Id: I54f8dde6d6c6acd8b4f5b92f985a2b66e2bd61e0

---
+++ routes/random.js
@@ -0,0 +1,47 @@
+/**
+ * random-card-single returns information about single random article suited
+ * to card-type presentations.
+ */
+
+'use strict';
+
+var mUtil = require('../lib/mobile-util');
+var sUtil = require('../lib/util');
+var randomPage = require('../lib/feed/random');
+
+/**
+ * The main router object
+ */
+var router = sUtil.router();
+
+/**
+ * The main application object reported when this module is require()d
+ */
+var app;
+
+/**
+ * GET {domain}/v1/page/random/summary
+ * Returns a single random result well suited to card-type layouts, i.e.
+ * one likely to have an image url, text extract and wikidata description.
+ *
+ * Multiple random items are requested, but only the result having
+ * the highest relative score is returned. Requesting about 12 items
+ * seems to consistently produce a really "good" result.
+ */
+router.get('/random/summary', function (req, res) {
+    return randomPage.promise(app, req)
+    .then(function (result) {
+        res.status(200);
+        mUtil.setETag(req, res, result.meta.etag);
+        res.json(result.payload).end();
+    });
+});
+
+module.exports = function (appObj) {
+    app = appObj;
+    return {
+        path: '/page',
+        api_version: 1,
+        router: router
+    };
+};
---
+++ test/features/random/pagecontent.js
@@ -0,0 +1,32 @@
+'use strict';
+
+var assert = require('../../utils/assert.js');
+var preq   = require('preq');
+var server = require('../../utils/server.js');
+var headers = require('../../utils/headers.js');
+
+describe('random', function() {
+    this.timeout(20000);
+
+    before(function () { return server.start(); });
+
+    it('should respond to GET request with expected headers, incl. CORS and CSP headers', function() {
+        return headers.checkHeaders(server.config.uri + 'en.wikipedia.org/v1/page/random/summary',
+            'application/json');
+    });
+
+    it('Random page should have expected properties', function() {
+        return preq.get({ uri: server.config.uri + 'de.wikipedia.org/v1/page/random/summary' })
+            .then(function(res) {
+                var lead = res.body.lead;
+                assert.deepEqual(res.status, 200);
+                assert.ok(res.body.title.length > 0, 'title should not be empty');
+                // It most likely has also a description, extract, and thumbnail, just not guaranteed
+
+                // We can check that there's a source element inside the thumbnail if we got one
+                if (res.body.thumbnail) {
+                    assert.ok(res.body.thumbnail.source.length > 0, 'thumbnail.source should not be empty');
+                }
+            });
+    });
+});
