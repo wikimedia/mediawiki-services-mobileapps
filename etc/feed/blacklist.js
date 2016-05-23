bb3e91dbf412049bfb67caeec8a2091514d34c5b
Michael Holloway <mholloway@wikimedia.org>
2016-05-23 16:30:43 -0400

    Most-read articles endpoint
    Returns the raw and normalized titles, pageid, rank, number of views,
    and (if available) a thumbnail and/or description for the top 40-50
    most-read articles for the date requested.
    
    http://localhost:6927/en.wikipedia.org/v1/page/most-read/2016/05/30
    
    Bug: T132766
    Change-Id: I9f8a494b59b3ef931e122d2ff4025bae120dba35

---
+++ etc/feed/blacklist.js
@@ -0,0 +1,17 @@
+/**
+ * A list of titles persistently in the most-read results but that appear to
+ * have few human viewers based on their traffic being almost all mobile or
+ * almost all non-mobile. See https://phabricator.wikimedia.org/T124716#2080637.
+ * TODO: Get this filtering or something like it into the upstream pageview API.
+ */
+var BLACKLIST = [
+    '-',
+    'Test_card',
+    'Web_scraping',
+    'XHamster',
+    'Java_(programming_language)',
+    'Images/upload/bel.jpg',
+    'Superintelligence:_Paths,_Dangers,_Strategies',
+];
+
+module.exports = BLACKLIST;
---
+++ test/lib/mobile-util/mobile-util-test.js
@@ -0,0 +1,46 @@
+'use strict';
+
+var assert = require('../../utils/assert');
+var mUtil = require('../../../lib/mobile-util');
+var preq  = require('preq');
+
+var obj1 = { hello: true, world: true };
+var obj2 = { goodbye: true, sea: true, again: false };
+var obj3 = { hello: true, world: true, again: false };
+var obj4 = { goodbye: true, sea: true };
+var obj5 = { goodbye: true, sea: true, world: true, again: true };
+var obj6 = { goodbye: true, sea: true, again: true };
+
+var arr1 = [ obj1, obj3 ];
+var arr2 = [ obj4, obj6 ];
+var arr3 = [ obj4, obj5 ];
+var arr4 = [ obj5, obj5 ];
+var arr5 = [ obj1, obj3, obj6 ];
+var arr6 = [ obj1, obj3 ];
+var arr7 = [ obj4, obj2 ];
+
+describe('lib:mobile-util', function() {
+    this.timeout(20000);
+
+    it('removeTLD should remove TLD', function() {
+        assert.deepEqual(mUtil.removeTLD('ru.wikipedia.org'), 'ru.wikipedia');
+    });
+
+    it('mergeByProp should merge two objects by shared property', function() {
+        mUtil.mergeByProp(arr1, arr2, 'again')
+        assert.deepEqual(arr1, arr5);
+    });
+
+    it('adjustMemberKeys should make the specified adjustments', function() {
+        mUtil.adjustMemberKeys(arr6, [['goodbye', 'hello'], ['sea', 'world']]);
+        assert.deepEqual(arr6, arr7);
+
+        mUtil.adjustMemberKeys(arr3, [['goodbye', 'hello'], ['sea', 'world']]);
+        assert.deepEqual(arr3, arr2);
+    });
+
+    it('fillInMemberKeys should make the specified adjustments', function() {
+        mUtil.fillInMemberKeys(arr3, [['world', 'goodbye'], ['again', 'sea']]);
+        assert.deepEqual(arr3, arr4);
+    })
+});
