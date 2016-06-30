387f514b302bf035e7158a75ffbaef4f846b9f6a
Mholloway <mholloway@wikimedia.org>
2016-06-30 12:53:40 +0000

    Revert "Hygiene: Remove mobile-text route"
    This reverts commit 181a20e5470a7936493f97068e0a1e8abc9afc52.
    
    It turns out this endpoint had at least one consumer outside WMF.  We should restore it now and, if we need to remove it again, announce our intent to do so first.
    
    Change-Id: Id0425bf7752d2e266a5ca769c19b903cd424852d

---
+++ test/features/mobile-text/pagecontent.js
@@ -0,0 +1,71 @@
+'use strict';
+
+var assert = require('../../utils/assert.js');
+var preq   = require('preq');
+var server = require('../../utils/server.js');
+var headers = require('../../utils/headers.js');
+
+describe('mobile-text', function() {
+    this.timeout(20000);
+
+    before(function () { return server.start(); });
+
+    it('should respond to GET request with expected headers, incl. CORS and CSP headers', function() {
+        return headers.checkHeaders(server.config.uri + 'en.wikipedia.org/v1/page/mobile-text/Foobar',
+            'application/json');
+    });
+    it('should have the right meta fields in the JSON response', function() {
+        return preq.get({ uri: server.config.uri + 'en.wikipedia.org/v1/page/mobile-text/Foobar' })
+            .then(function(res) {
+                assert.deepEqual(res.status, 200);
+                assert.notDeepEqual(res.body.lastmodified, undefined);
+                assert.notDeepEqual(res.body.revision, undefined);
+                assert.notDeepEqual(res.body.languagecount, undefined);
+                assert.notDeepEqual(res.body.id, undefined);
+                assert.notDeepEqual(res.body.protection, undefined);
+                assert.notDeepEqual(res.body.editable, undefined);
+                assert.deepEqual(res.body.displaytitle, 'Foobar');
+            });
+    });
+    it('should have the right structure of section objects', function() {
+        return preq.get({ uri: server.config.uri + 'en.wikipedia.org/v1/page/mobile-text/Foobar' })
+            .then(function(res) {
+                assert.deepEqual(res.status, 200);
+                assert.notDeepEqual(res.body.sections, undefined);
+                for (var i = 0; i < res.body.sections.length; i++) {
+                    assert.notDeepEqual(res.body.sections[i].id, undefined);
+                    assert.notDeepEqual(res.body.sections[i].items, undefined);
+                }
+            });
+    });
+    it('should have the right structure of paragraph, image, and video objects', function() {
+        return preq.get({ uri: server.config.uri + 'test.wikipedia.org/v1/page/mobile-text/LiteTest' })
+            .then(function(res) {
+                assert.deepEqual(res.status, 200);
+                var numParagraphs = 0;
+                var numImages = 0;
+                var numVideos = 0;
+                for (var i = 0; i < res.body.sections.length; i++) {
+                    var section = res.body.sections[i];
+                    for (var j = 0; j < section.items.length; j++) {
+                        var item = section.items[j];
+                        if (item.type === 'p') {
+                            assert.notDeepEqual(item.text, undefined);
+                            numParagraphs++;
+                        } else if (item.type === 'image') {
+                            assert.notDeepEqual(item.src, undefined);
+                            assert.notDeepEqual(item.name, undefined);
+                            numImages++;
+                        } else if (item.type === 'video') {
+                            assert.notDeepEqual(item.src, undefined);
+                            assert.notDeepEqual(item.name, undefined);
+                            numVideos++;
+                        }
+                    }
+                }
+                assert.notDeepEqual(numParagraphs, 0);
+                assert.notDeepEqual(numImages, 0);
+                assert.notDeepEqual(numVideos, 0);
+            });
+    });
+});
