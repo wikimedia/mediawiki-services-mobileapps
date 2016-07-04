36a3d6748b681f410c948b499e2e7a630bd556d7
Michael Holloway <mholloway@wikimedia.org>
2016-07-04 19:12:23 -0400

    Validate input dates prior to processing
    Adds a function to check that dates are valid and in the correct format
    before further processing and uses it where applicable.
    
    Bug: T139315
    Change-Id: Iaf6d5feac1c1d6b7f8ebc3032af6acd7bc491efe

--- test/lib/dateUtil/date-util-test.js
+++ test/lib/dateUtil/date-util-test.js
@@ -2,11 +2,12 @@
 
 var assert = require('../../utils/assert.js');
 var dateUtil = require('../../../lib/dateUtil');
+var HTTPError = require('../../../lib/util').HTTPError;
 
 describe('lib:dateUtil', function() {
     this.timeout(20000);
 
-    it('getRequestedDate(2016/04/15) should return a valid Date object', function() {
+    it('getRequestedDate(2016-04-15) should return a valid Date object', function() {
         var actual = dateUtil.getRequestedDate({
             params: {
                 yyyy: 2016,
@@ -18,4 +19,29 @@ describe('lib:dateUtil', function() {
         assert.equal(actual.getUTCMonth(), 4 - 1);
         assert.equal(actual.getUTCDate(), 15);
     });
+
+    it('date format validation should reject invalid formats', function() {
+        assert.throws(function() { dateUtil.validate('2016-7-4'); }, HTTPError);
+        assert.throws(function() { dateUtil.validate('2016-07-4'); }, HTTPError);
+        assert.throws(function() { dateUtil.validate('2016-7-04'); }, HTTPError);
+        assert.throws(function() { dateUtil.validate('00000002016-07-04'); }, HTTPError);
+        assert.throws(function() { dateUtil.validate('2016-000000007-04'); }, HTTPError);
+        assert.throws(function() { dateUtil.validate('2016-07-000000004'); }, HTTPError);
+        assert.throws(function() { dateUtil.validate('2039-07-04'); }, HTTPError);
+        assert.throws(function() { dateUtil.validate('2016-13-04'); }, HTTPError);
+        assert.throws(function() { dateUtil.validate('2016-07-34'); }, HTTPError);
+        assert.throws(function() { dateUtil.validate('0000-07-04'); }, HTTPError);
+        assert.throws(function() { dateUtil.validate('2016-00-04'); }, HTTPError);
+        assert.throws(function() { dateUtil.validate('2016-07-00'); }, HTTPError);
+        assert.throws(function() { dateUtil.validate('abcd-07-04'); }, HTTPError);
+        assert.throws(function() { dateUtil.validate('2016-ef-04'); }, HTTPError);
+        assert.throws(function() { dateUtil.validate('2016-07-gh'); }, HTTPError);
+        assert.throws(function() { dateUtil.validate('*!@#-07-04'); }, HTTPError);
+        assert.throws(function() { dateUtil.validate('2016-*!-04'); }, HTTPError);
+        assert.throws(function() { dateUtil.validate('2016-07-@#'); }, HTTPError);
+        assert.throws(function() { dateUtil.validate('\u00002016-07-04'); }, HTTPError);
+        assert.throws(function() { dateUtil.validate('2016-\u000007-04'); }, HTTPError);
+        assert.throws(function() { dateUtil.validate('2016-07-\u000004'); }, HTTPError);
+        assert.doesNotThrow(function() { dateUtil.validate('2016-07-04'); });
+    })
 });
