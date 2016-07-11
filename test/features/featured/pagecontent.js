b919b23b5c5a9121df739a84bdc63f2f94efe26e
Michael Holloway <mholloway@wikimedia.org>
2016-07-11 11:51:35 -0400

    Add mw-format-encoded title to random & featured article endpoints
    We need to include the raw, non-normalized page title in the random
    endpoint response(s) in order to avoid excessive redirects when getting
    the full page metadata from RESTBase.  But MediaWiki only gives us a list
    of normalized titles when we request a list of random pages.
    
    This patch leverages the mediawiki-title library to derive the raw, DB-
    encoded title format based on the request site's siteinfo and includes it
    in the random endpoint response.
    
    It's also added to the featured article endpoint, which uses the same
    mwapi.buildSummaryResponse function to build its output.
    
    See https://github.com/wikimedia/restbase/pull/636#issuecomment-231385240.
    
    Bug: T132597
    Change-Id: I5eb24688a774316481b98a494d3984044d09d948

--- test/features/featured/pagecontent.js
+++ test/features/featured/pagecontent.js
@@ -20,7 +20,8 @@ describe('featured', function() {
                 assert.status(res, 200);
                 // the page id should be stable but not the revision:
                 assert.ok(res.headers.etag.indexOf('50089449/') == 0);
-                assert.equal(res.body.title, 'Cosmic Stories and Stirring Science Stories');
+                assert.equal(res.body.title, 'Cosmic_Stories_and_Stirring_Science_Stories');
+                assert.equal(res.body.normalizedtitle, 'Cosmic Stories and Stirring Science Stories');
                 assert.equal(res.body.thumbnail.source, 'https://upload.wikimedia.org/wikipedia/commons/1/19/Cosmic_Science-Fiction_May_1941.jpg');
                 assert.ok(res.body.extract.indexOf('Cosmic Stories ') >= 0);
             });
@@ -32,7 +33,8 @@ describe('featured', function() {
                 assert.status(res, 200);
                 // the page id should be stable but not the revision:
                 assert.ok(res.headers.etag.indexOf('50282338/') == 0);
-                assert.equal(res.body.title, 'Lightning (Final Fantasy)');
+                assert.equal(res.body.title, 'Lightning_(Final_Fantasy)');
+                assert.equal(res.body.normalizedtitle, 'Lightning (Final Fantasy)');
                 assert.ok(res.body.description.indexOf('Final Fantasy') >= 0);
                 assert.ok(res.body.extract.indexOf('Lightning ') >= 0);
             });
