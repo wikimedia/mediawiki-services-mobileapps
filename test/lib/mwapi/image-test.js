313692de710c5b16c54f7e72d55be6a4fb70fbf4
Michael Holloway <mholloway@wikimedia.org>
2016-07-12 10:40:38 -0400

    Hygiene: Consolidate lib/mwapi.js constants & functions in export object
    Change-Id: Ie15bd921c63ff32d4fcb1112268793f1b0edb0d5

--- test/lib/mwapi/image-test.js
+++ test/lib/mwapi/image-test.js
@@ -7,7 +7,7 @@ describe('lib:mwapi:image', function () {
     this.timeout(20000);
 
     it('buildLeadImageUrls("a") should return all "a"s', function () {
-        assert.deepEqual(mwapi._buildLeadImageUrls('a'), {
+        assert.deepEqual(mwapi.buildLeadImageUrls('a'), {
                 320: 'a',
                 640: 'a',
                 800: 'a',
@@ -17,7 +17,7 @@ describe('lib:mwapi:image', function () {
     });
 
     it('buildLeadImageUrls with size 1024px', function () {
-        assert.deepEqual(mwapi._buildLeadImageUrls('//upload.wikimedia.org/wikipedia/commons/thumb/a/a0/Sun_in_February.jpg/1024px-Sun_in_February.jpg'), {
+        assert.deepEqual(mwapi.buildLeadImageUrls('//upload.wikimedia.org/wikipedia/commons/thumb/a/a0/Sun_in_February.jpg/1024px-Sun_in_February.jpg'), {
                 320: '//upload.wikimedia.org/wikipedia/commons/thumb/a/a0/Sun_in_February.jpg/320px-Sun_in_February.jpg',
                 640: '//upload.wikimedia.org/wikipedia/commons/thumb/a/a0/Sun_in_February.jpg/640px-Sun_in_February.jpg',
                 800: '//upload.wikimedia.org/wikipedia/commons/thumb/a/a0/Sun_in_February.jpg/800px-Sun_in_February.jpg',
@@ -27,7 +27,7 @@ describe('lib:mwapi:image', function () {
     });
 
     it('buildLeadImageUrls with size 555px should return size 320 for 320 and then 555 for rest', function () {
-        assert.deepEqual(mwapi._buildLeadImageUrls('//upload.wikimedia.org/wikipedia/commons/thumb/a/a0/Sun_in_February.jpg/555px-Sun_in_February.jpg'), {
+        assert.deepEqual(mwapi.buildLeadImageUrls('//upload.wikimedia.org/wikipedia/commons/thumb/a/a0/Sun_in_February.jpg/555px-Sun_in_February.jpg'), {
                 320: '//upload.wikimedia.org/wikipedia/commons/thumb/a/a0/Sun_in_February.jpg/320px-Sun_in_February.jpg',
                 640: '//upload.wikimedia.org/wikipedia/commons/thumb/a/a0/Sun_in_February.jpg/555px-Sun_in_February.jpg',
                 800: '//upload.wikimedia.org/wikipedia/commons/thumb/a/a0/Sun_in_February.jpg/555px-Sun_in_February.jpg',
@@ -37,7 +37,7 @@ describe('lib:mwapi:image', function () {
     });
 
     it('buildLeadImageUrls with size 750px should return size 320, 640, and then 750 for rest', function () {
-        assert.deepEqual(mwapi._buildLeadImageUrls('//upload.wikimedia.org/wikipedia/commons/thumb/a/a0/Sun_in_February.jpg/750px-Sun_in_February.jpg'), {
+        assert.deepEqual(mwapi.buildLeadImageUrls('//upload.wikimedia.org/wikipedia/commons/thumb/a/a0/Sun_in_February.jpg/750px-Sun_in_February.jpg'), {
                 320: '//upload.wikimedia.org/wikipedia/commons/thumb/a/a0/Sun_in_February.jpg/320px-Sun_in_February.jpg',
                 640: '//upload.wikimedia.org/wikipedia/commons/thumb/a/a0/Sun_in_February.jpg/640px-Sun_in_February.jpg',
                 800: '//upload.wikimedia.org/wikipedia/commons/thumb/a/a0/Sun_in_February.jpg/750px-Sun_in_February.jpg',
@@ -46,7 +46,7 @@ describe('lib:mwapi:image', function () {
         );
     });
     it('buildLeadImageUrls with size 200px should return size 200 for all URLs', function () {
-        assert.deepEqual(mwapi._buildLeadImageUrls('//upload.wikimedia.org/wikipedia/commons/thumb/a/a0/Sun_in_February.jpg/200px-Sun_in_February.jpg'), {
+        assert.deepEqual(mwapi.buildLeadImageUrls('//upload.wikimedia.org/wikipedia/commons/thumb/a/a0/Sun_in_February.jpg/200px-Sun_in_February.jpg'), {
                 320: '//upload.wikimedia.org/wikipedia/commons/thumb/a/a0/Sun_in_February.jpg/200px-Sun_in_February.jpg',
                 640: '//upload.wikimedia.org/wikipedia/commons/thumb/a/a0/Sun_in_February.jpg/200px-Sun_in_February.jpg',
                 800: '//upload.wikimedia.org/wikipedia/commons/thumb/a/a0/Sun_in_February.jpg/200px-Sun_in_February.jpg',
