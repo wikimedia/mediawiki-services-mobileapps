7b10b31b1fd6078311d7fd7a0a4975d2df42629e
Dmitry Brant <dbrant@wikimedia.org>
2016-07-21 13:02:13 -0400

    Enable In the News for a few more languages.
    This enables the News endpoint for additional languages of German,
    Russian, and Hebrew, since these Wikipedias use a very similar template
    for presenting News content on their Main Pages. So, why not?
    
    This also tweaks the selector that gathers the anchor tags from the text,
    so that it only looks for WikiLinks. Certain news stories, particularly
    in Hebrew Wikipedia, include external links that should not be broken
    out into our response.
    
    Change-Id: Ie365300fd5290711ce2664a57e2d283213219c7d

--- test/features/news/news.js
+++ test/features/news/news.js
@@ -19,29 +19,31 @@ describe('in the news', function() {
 
     before(function () { return server.start(); });
 
-    it('should respond to GET request with expected headers, incl. CORS and CSP headers', function() {
-        return headers.checkHeaders(server.config.uri + 'en.wikipedia.org/v1/page/news',
-            'application/json');
-    });
-    it('results list should have expected properties', function() {
-        return preq.get({ uri: server.config.uri + 'en.wikipedia.org/v1/page/news' })
-          .then(function(res) {
-              assert.deepEqual(res.status, 200);
-              assert.ok(res.body.length);
-              res.body.forEach(function (elem) {
-                  assert.ok(elem.story, 'story should be present');
-                  assert.ok(elem.links, 'links should be present');
+    [ 'de', 'en', 'he', 'ru' ].forEach(function(lang) {
+        it(lang + ': should respond to GET request with expected headers, incl. CORS and CSP headers', function () {
+            return headers.checkHeaders(server.config.uri + lang + '.wikipedia.org/v1/page/news',
+                'application/json');
+        });
+        it(lang + ': results list should have expected properties', function () {
+            return preq.get({uri: server.config.uri + lang + '.wikipedia.org/v1/page/news'})
+                .then(function (res) {
+                    assert.deepEqual(res.status, 200);
+                    assert.ok(res.body.length);
+                    res.body.forEach(function (elem) {
+                        assert.ok(elem.story, 'story should be present');
+                        assert.ok(elem.links, 'links should be present');
 
-                  elem.links.forEach(function(link) {
-                      assert.ok(link.pageid, 'page id should be present');
-                      assert.ok(link.ns !== undefined, 'namespace should be present'); // 0 is falsey but good
-                      assert.ok(link.title, 'title should be present');
-                      assert.ok(link.normalizedtitle, 'normalized title should be present');
-                      if (link.thumbnail) {
-                          assert.ok(link.thumbnail.source, 'thumbnail should have source URL');
-                      }
-                  });
-              });
-          });
+                        elem.links.forEach(function (link) {
+                            assert.ok(link.pageid, 'page id should be present >>> ' + JSON.stringify(link));
+                            assert.ok(link.ns !== undefined, 'namespace should be present'); // 0 is falsey but good
+                            assert.ok(link.title, 'title should be present');
+                            assert.ok(link.normalizedtitle, 'normalized title should be present');
+                            if (link.thumbnail) {
+                                assert.ok(link.thumbnail.source, 'thumbnail should have source URL');
+                            }
+                        });
+                    });
+                });
+        });
     });
 });
