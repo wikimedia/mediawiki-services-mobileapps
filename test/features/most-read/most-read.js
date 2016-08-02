32397096fb25c32296b050d60e64b3a1f3e19fec
Michael Holloway <mholloway@wikimedia.org>
2016-08-02 15:03:16 +0000

    Most-read endpoint: Handle MWAPI responses without normalizations
    For some languges (Asian languages, for instance) it will be reasonably
    common for there to be no titles with normalizations in the list of most-
    read articles.  Handle these.
    
    Bug: T141833
    Change-Id: I697daa6cb0b044dbb1ecedfd45393c9c3177db8f

--- test/features/most-read/most-read.js
+++ test/features/most-read/most-read.js
@@ -38,6 +38,24 @@ describe('most-read articles', function() {
               });
           });
     });
+    it('should load successfully even with no normalizations from the MW API', function() {
+        return preq.get({ uri: server.config.uri + 'ja.wikipedia.org/v1/page/most-read/2016/06/15' })
+          .then(function(res) {
+              assert.deepEqual(res.status, 200);
+              assert.ok(res.body.date);
+              assert.ok(res.body.articles.length);
+              res.body.articles.forEach(function (elem) {
+                  assert.ok(elem.title, 'title should be present');
+                  assert.ok(elem.normalizedtitle, 'normalizedtitle should be present');
+                  assert.ok(elem.views, 'views should be present');
+                  assert.ok(elem.rank, 'rank should be present');
+                  assert.ok(elem.pageid, 'pageid should be present');
+                  assert.ok(BLACKLIST.indexOf(elem.title) === -1, 'Should not include blacklisted title');
+                  assert.ok(elem.title !== 'Main_Page', 'Should not include the Main Page');
+                  assert.ok(elem.title.indexOf('Special:') === -1, 'Should not include Special page');
+              });
+          });
+    });
     it('Request to mobile domain should complete successfully', function() {
         return preq.get({ uri: server.config.uri + 'en.m.wikipedia.org/v1/page/most-read/' + dateString })
           .then(function(res) {
