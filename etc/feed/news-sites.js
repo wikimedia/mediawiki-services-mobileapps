28b969e172f50eef7b0f9ccb4ccfd3658240182f
Stephen Niedzielski <stephen@niedzielski.com>
2016-11-22 16:18:48 -0700

    Hygiene: add model for news sites
    Bug: T148444
    Change-Id: I6dd6ec2f97d8688518907ef31af8654f83545109

--- etc/feed/news-sites.js
+++ etc/feed/news-sites.js
@@ -1,32 +1,55 @@
 'use strict';
 
 /**
- * A hash map of (Wikipedia site) languages codes to objects with
- * information to control how to get news items for this particular
- * Wikipedia site.
- * The object consists of a title and a selector.
- * 1) The title is the page title used to scrape the news items from.
- * 2) The selector is used to query news headlines, usually found in the
- *    first 'ul' but there are exceptions, often because the first <ul>
- *    contains template instructions instead of the actual news items,
- *    or instead of a <ul> Parsoid uses a <div>. This selector should
- *    not include specific categories like deaths, sports, etc.
+ * @private {!string} _title
+ * @private {!string} _headlineSelector
+ */
+class NewsSite {
+    /**
+     * @param {!string} title
+     * @param {!string} headlineSelector
+     */
+    constructor(title, headlineSelector) {
+        this._title = title;
+        this._headlineSelector = headlineSelector;
+    }
+
+    /**
+     * @return {!string} News source wiki portal or template title to scrape
+     *                   data from
+     */
+    get title() {
+        return this._title;
+    }
+
+    /**
+     * @return {!string} Query selector for news headline containers (not
+     *                   specific categories like deaths, sports, etc)
+     */
+    get headlineSelector() {
+        return this._headlineSelector;
+    }
+}
+
+/**
+ * @type {{Object.<string, NewsSite>}} A map of Wikipedia site languages codes
+ *                                     to NewsSites
  */
 module.exports = {
-    da: { title: 'Skabelon:Forside_aktuelle_begivenheder', headlineSelector: 'div > li' },
-    de: { title: 'Wikipedia:Hauptseite/Aktuelles', headlineSelector: 'li' },
-    el: { title: 'Πύλη:Τρέχοντα_γεγονότα/Επικεφαλίδες', headlineSelector: 'li' },
-    en: { title: 'Template:In_the_news', headlineSelector: 'ul[id^=mw] li' },
-    es: { title: 'Portal:Actualidad',
-        headlineSelector: 'table:nth-of-type(1) > tbody > tr > td > ul:nth-of-type(1) > li' },
-    fi: { title: 'Malline:Uutisissa', headlineSelector: 'body > ul > li' },
-    fr: { title: 'Modèle:Accueil_actualité', headlineSelector: 'div ul[id^=mw] > li' },
-    he: { title: 'תבנית:חדשות_ואקטואליה', headlineSelector: 'body > ul > li' },
-    ko: { title: '틀:새로_들어온_소식', headlineSelector: 'body > ul > li' },
-    no: { title: 'Mal:Aktuelt', headlineSelector: 'ul > li' },
-    pl: { title: 'Szablon:Aktualności', headlineSelector: 'ul:last-of-type > li' },
-    pt: { title: 'Portal:Eventos_atuais', headlineSelector: 'div > ul > li' },
-    ru: { title: 'Шаблон:Актуальные_события', headlineSelector: 'body > ul > li' },
-    sv: { title: 'Portal:Huvudsida/Aktuella händelser', headlineSelector: 'body > ul > li' },
-    vi: { title: 'Bản_mẫu:Tin_tức', headlineSelector: 'ul > li' }
+    da: new NewsSite('Skabelon:Forside_aktuelle_begivenheder', 'div > li'),
+    de: new NewsSite('Wikipedia:Hauptseite/Aktuelles', 'li'),
+    el: new NewsSite('Πύλη:Τρέχοντα_γεγονότα/Επικεφαλίδες', 'li'),
+    en: new NewsSite('Template:In_the_news', 'ul[id^=mw] li'),
+    es: new NewsSite('Portal:Actualidad',
+        'table:nth-of-type(1) > tbody > tr > td > ul:nth-of-type(1) > li'),
+    fi: new NewsSite('Malline:Uutisissa', 'body > ul > li'),
+    fr: new NewsSite('Modèle:Accueil_actualité', 'div ul[id^=mw] > li'),
+    he: new NewsSite('תבנית:חדשות_ואקטואליה', 'body > ul > li'),
+    ko: new NewsSite('틀:새로_들어온_소식', 'body > ul > li'),
+    no: new NewsSite('Mal:Aktuelt', 'ul > li'),
+    pl: new NewsSite('Szablon:Aktualności', 'ul:last-of-type > li'),
+    pt: new NewsSite('Portal:Eventos_atuais', 'div > ul > li'),
+    ru: new NewsSite('Шаблон:Актуальные_события', 'body > ul > li'),
+    sv: new NewsSite('Portal:Huvudsida/Aktuella händelser', 'body > ul > li'),
+    vi: new NewsSite('Bản_mẫu:Tin_tức', 'ul > li')
 };
