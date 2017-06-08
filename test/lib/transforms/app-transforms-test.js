76d78cdbabeb348bd0bbee0f24a594c2c2d1df87
BearND <bsitzmann@wikimedia.org>
2017-06-07 20:47:01 -0600

    Limit video transform to videos
    This is a follow-up to Ic45c0254.
    
    Parsoid uses video tags for both video and audio files. The previous
    logic did not distinguish between video and audio files and treated
    them the same. This was incorrect since audio files are to be played
    embedded while videos go to the Gallery.
    
    Bug: T167183
    Bug: T164522
    Change-Id: Iac99237d97ccdc76141d9f9ae0b783dfcd76adf6

--- test/lib/transforms/app-transforms-test.js
+++ test/lib/transforms/app-transforms-test.js
@@ -7,10 +7,39 @@ const a = require('../../../lib/transformations/anchorPopUpMediaTransforms');
 const html = fs.readFileSync(`${__dirname}/../bill-clinton.html`, 'utf-8');
 
 describe('lib:app-transforms', () => {
-    it('fixVideoAnchor should apply app_media class to all video anchors', () => {
+    it('fixVideoAnchor should skip video tags just holding audio', () => {
+        const doc = domino.createDocument(`
+<div><span typeof="mw:Audio"><span>
+        <video
+                controls=""
+                preload="none">
+                <source
+                        src="https://upload.wikimedia.org/wikipedia/en/c/c4/Radiohead_-_Creep_%28sample%29.ogg"
+                        type='audio/ogg; codecs="vorbis"'/>
+        </video>
+</span></span></div>`);
+        a.fixVideoAnchor(doc);
+        const videoThumbImgElements = doc.querySelectorAll('a.app_media');
+        assert.equal(videoThumbImgElements.length, 0, 'Should not have marked the audio file');
+    });
+
+    it('fixVideoAnchor should transform actual videos', () => {
+        const doc = domino.createDocument(`
+<figure typeof="mw:Video/Thumb mw:Placeholder" id="mwBw"><span id="mwCA">
+    <video controls="">
+        <source src="https://upload.wikimedia.org/wikipedia/commons/9/96/Curiosity%27s_Seven_Minutes_of_Terror.ogv"
+        type='video/ogg; codecs="theora, vorbis"' />
+    </video>
+</span></figure>`);
+        a.fixVideoAnchor(doc);
+        const videoThumbImgElements = doc.querySelectorAll('a.app_media');
+        assert.equal(videoThumbImgElements.length, 0, 'Should have marked the video file');
+    });
+
+    it('fixVideoAnchor should apply app_media class to video anchors', () => {
         const doc = domino.createDocument(html);
         a.fixVideoAnchor(doc);
         const videoThumbImgElements = doc.querySelectorAll('a.app_media');
-        assert.equal(videoThumbImgElements.length, 6, 'Failed to find app_media classes');
+        assert.equal(videoThumbImgElements.length, 3, 'Failed to find app_media classes');
     });
 });
