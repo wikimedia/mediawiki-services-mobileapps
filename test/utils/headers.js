f3067226aa4f4f4ecc3bddc610680dde4b1edc2c
BearND <bsitzmann@wikimedia.org>
2017-08-16 11:17:44 -0600

    Allow custom content types
    Previously we only had application/json content types.
    Now we also allow other types.
    To keep the changes small application/json is the default.
    
    Change-Id: I12b48900c02eb8c8695cac591814fe8ccb047b18

--- test/utils/headers.js
+++ test/utils/headers.js
@@ -4,13 +4,15 @@
 const assert = require('./assert.js');
 const preq   = require('preq');
 
+const PROFILE_REGEX = 'profile="https://www.mediawiki.org/wiki/Specs/[A-Za-z-]+/\\d+\\.\\d+\\.\\d+"$';
+const JSON_CONTENT_TYPE_REGEX = `^application/json; charset=utf-8; ${PROFILE_REGEX}`;
+const HTML_CONTENT_TYPE_REGEX = `^text/html; charset=utf-8; ${PROFILE_REGEX}`;
 
 function checkHeaders(uri, expContentType) {
     return preq.get({ uri })
         .then((res) => {
             assert.deepEqual(res.status, 200);
-            expContentType = expContentType || '^application/json; charset=utf-8; '
-                + 'profile="https://www.mediawiki.org/wiki/Specs/[a-z-]+/\\d+\\.\\d+\\.\\d+"$';
+            expContentType = expContentType || JSON_CONTENT_TYPE_REGEX;
             assert.contentType(res, expContentType);
             assert.deepEqual(res.headers.etag, '^"[^/"]+/[^/"]+"$',
                 'The ETag header is not present or invalid');
@@ -31,5 +33,6 @@ function checkHeaders(uri, expContentType) {
 
 
 module.exports = {
+    HTML_CONTENT_TYPE_REGEX,
     checkHeaders
 };
