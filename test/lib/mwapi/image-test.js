e746aea8434b051e61b85a50e7465a39311e8109
Michael Holloway <mholloway@wikimedia.org>
2017-09-20 10:23:01 -0400

    Rewrite lead image URLs to https and bump mobile-sections to 0.12.4
    Bug: T176263
    Change-Id: I7a70ef3eddda5f7b013871487a4eddfdf67ed575

--- test/lib/mwapi/image-test.js
+++ test/lib/mwapi/image-test.js
@@ -4,6 +4,8 @@ const assert = require('../../utils/assert.js');
 const buildUrls = require('../../../lib/mwapi').buildLeadImageUrls;
 
 const path = '//upload.wikimedia.org/wikipedia/commons/thumb/a/a0';
+const httpPath = 'http://upload.wikimedia.org/wikipedia/commons/thumb/a/a0';
+const httpsPath = 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a0';
 
 describe('lib:mwapi:image', () => {
     it('buildLeadImageUrls("a") should return all "a"s', () => {
@@ -45,12 +47,23 @@ describe('lib:mwapi:image', () => {
         }
         );
     });
+
     it('buildLeadImageUrls with size 200px should return size 200 for all URLs', () => {
         assert.deepEqual(buildUrls(`${path}/Sun_in_February.jpg/200px-Sun_in_February.jpg`), {
             320: `${path}/Sun_in_February.jpg/200px-Sun_in_February.jpg`,
             640: `${path}/Sun_in_February.jpg/200px-Sun_in_February.jpg`,
             800: `${path}/Sun_in_February.jpg/200px-Sun_in_February.jpg`,
             1024: `${path}/Sun_in_February.jpg/200px-Sun_in_February.jpg`
+        }
+        );
+    });
+
+    it('buildLeadImageUrls rewrites http URLs to https', () => {
+        assert.deepEqual(buildUrls(`${httpPath}/Sun_in_February.jpg/1024px-Sun_in_February.jpg`), {
+            320: `${httpsPath}/Sun_in_February.jpg/320px-Sun_in_February.jpg`,
+            640: `${httpsPath}/Sun_in_February.jpg/640px-Sun_in_February.jpg`,
+            800: `${httpsPath}/Sun_in_February.jpg/800px-Sun_in_February.jpg`,
+            1024: `${httpsPath}/Sun_in_February.jpg/1024px-Sun_in_February.jpg`
         }
         );
     });
