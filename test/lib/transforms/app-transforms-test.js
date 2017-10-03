6b7a264ddb604cc7dcf580c1275543fd50edff4d
Arlo Breault <abreault@wikimedia.org>
2017-10-03 15:59:03 -0400

    Account for <figure-inline> instead of <span> for inline figures
     * Note that there're a number of fixtures that might be prudent to
       update as well.
    
    Bug: T177301
    Change-Id: Ideccf1ee265ee2ff9caed4e1ef4d129a1698f0a7

--- test/lib/transforms/app-transforms-test.js
+++ test/lib/transforms/app-transforms-test.js
@@ -9,7 +9,7 @@ const html = fs.readFileSync(`${__dirname}/../bill-clinton.html`, 'utf-8');
 describe('lib:app-transforms', () => {
     it('fixVideoAnchor should skip video tags just holding audio', () => {
         const doc = domino.createDocument(`
-<div><span typeof="mw:Audio"><span>
+<div><figure-inline typeof="mw:Audio"><span>
         <video
                 controls=""
                 preload="none">
@@ -17,7 +17,7 @@ describe('lib:app-transforms', () => {
                         src="https://upload.wikimedia.org/wikipedia/en/c/c4/Radiohead_-_Creep_%28sample%29.ogg"
                         type='audio/ogg; codecs="vorbis"'/>
         </video>
-</span></span></div>`);
+</span></figure-inline></div>`);
         a.fixVideoAnchor(doc);
         const videoThumbImgElements = doc.querySelectorAll('a.app_media');
         assert.equal(videoThumbImgElements.length, 0, 'Should not have marked the audio file');
