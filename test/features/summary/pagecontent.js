56e9c8e56cdcef383293408f2f36b9624c70d38d
Michael Holloway <mholloway@wikimedia.org>
2017-11-08 12:35:05 -0500

    Move namespace_id and namespace_text to top level
    Bug: T177431
    Change-Id: I31fecfc4d15f18003ea9f0f168ea9a32a2a0d626

--- test/features/summary/pagecontent.js
+++ test/features/summary/pagecontent.js
@@ -1,14 +1,9 @@
 'use strict';
 
 const preq = require('preq');
-const Title = require('mediawiki-title').Title;
 const assert = require('../../utils/assert.js');
 const headers = require('../../utils/headers.js');
 const server = require('../../utils/server.js');
-const mUtil = require('../../../lib/mobile-util');
-
-const blaLead = require('../../fixtures/formatted-lead-Β-lactam_antibiotic.json');
-const siteinfo = require('../../fixtures/siteinfo_enwiki.json');
 
 describe('summary', function() {
 
@@ -26,7 +21,6 @@ describe('summary', function() {
     });
 
     it('should respond with expected properties in payload', () => {
-
         const uri = localUri('Foobar/798652007');
         return preq.get({ uri })
             .then((res) => {
@@ -36,20 +30,8 @@ describe('summary', function() {
                 assert.deepEqual(res.body.titles.title, "Foobar");
                 assert.deepEqual(res.body.titles.normalized_title, "Foobar");
                 assert.deepEqual(res.body.titles.display_title, "Foobar");
-                assert.deepEqual(res.body.titles.namespace_id, 0);
-                assert.deepEqual(res.body.titles.namespace_name, "");
                 assert.ok(res.body.extract.indexOf('foobar') > -1);
                 assert.ok(res.body.extract_html.indexOf('<b>foobar</b>') > -1);
             });
-    });
-
-    it('titles dictionary contains all required fields', () => {
-        const title = Title.newFromText('Β-lactam_antibiotic', siteinfo);
-        const result = mUtil.buildTitleDictionary(title, blaLead);
-        assert.deepEqual(result.title, "Β-lactam_antibiotic");
-        assert.deepEqual(result.normalized_title, "Β-lactam antibiotic");
-        assert.deepEqual(result.display_title, "β-lactam antibiotic");
-        assert.deepEqual(result.namespace_id, 0);
-        assert.deepEqual(result.namespace_name, "");
     });
 });
