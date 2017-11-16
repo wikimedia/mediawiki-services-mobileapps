1898a61a088b0424779ffac7b96466d7d2479f7d
Michael Holloway <mholloway@wikimedia.org>
2017-11-16 13:00:55 -0500

    Media: incorporate initial data from page HTML
    Adds logic to incorporate data from the page HTML when generating a
    media item list via querySelectorAll.  Adds 'type', 'start_time',
    and 'end_time' via this path.
    
    As a bonus, since an initial array from querySelectorAll is maintained
    throughout processing, and results of the follow-up API call for metadata
    are simply merged in, there's no need for a follow-up sort.
    
    Bug: T177430
    Change-Id: Ib183edde7c1ecfabe2f37c4289c0471616e2572c

--- routes/media.js
+++ routes/media.js
@@ -1,11 +1,13 @@
 'use strict';
 
 const BBPromise = require('bluebird');
+const domino = require('domino');
 const mUtil = require('../lib/mobile-util');
 const parsoid = require('../lib/parsoid-access');
 const sUtil = require('../lib/util');
 const mwapi = require('../lib/mwapi');
 const media = require('../lib/media');
+const Title = require('mediawiki-title').Title;
 
 const router = sUtil.router();
 let app;
@@ -16,25 +18,29 @@ let app;
  */
 router.get('/media/:title', (req, res) => {
     return parsoid.getParsoidHtml(app, req).then((response) => {
-        const titles = media.getTitles(response.body);
-        if (!titles) {
+        const headers = response.headers;
+        const doc = domino.createDocument(response.body);
+        // todo: handle Mathoid-rendered math images
+        const selection = doc.querySelectorAll('*[typeof^=mw:Image] img,*[typeof^=mw:Video] video');
+        if (!selection) {
             res.send({ items: [] });
             return;
         }
+        const mediaList = media.getMediaItemInfoFromPage(selection);
+        const titles = mediaList.map(item => item.title);
         return BBPromise.props({
-            titles,
-            metadata: media.getMetadata(app, req, titles),
-            headers: response.headers,
+            metadata: media.getMetadataFromApi(app, req, titles),
             siteinfo: mwapi.getSiteInfo(app, req)
         }).then((response) => {
-            const revTid = parsoid.getRevAndTidFromEtag(response.headers);
+            const revTid = parsoid.getRevAndTidFromEtag(headers);
             const metadataList = response.metadata.items;
-            const result = metadataList.filter((item) => {
+            metadataList.forEach((item) => {
+                item.title = Title.newFromText(item.title, response.siteinfo).getPrefixedDBKey();
+            });
+            mUtil.mergeByProp(mediaList, metadataList, 'title', false);
+            const result = mediaList.filter((item) => {
                 return media.filterResult(item);
             });
-            if (result) {
-                media.sort(response.titles, result, response.siteinfo);
-            }
             mUtil.setETag(res, revTid.revision, revTid.tid);
             mUtil.setContentType(res, mUtil.CONTENT_TYPES.unpublished);
             res.send({ items: result });
