350528bdbf4d16d2746d643f3e8bf2e909f818c9
BearND <bsitzmann@wikimedia.org>
2017-11-29 11:59:17 -0700

    Tweak Big English fundraiser copy and start time
    Now with double the line breaks and updated button label.
    Postponed the start time to Wednesday, 12/06
    since I have not heard about the final approval of things yet.
    
    Bug: T181004
    Change-Id: Icc8485f603b8c1aae2b3af4d0e58a1b9e6c23854

--- etc/feed/announcements.js
+++ etc/feed/announcements.js
@@ -7,7 +7,7 @@ const AnnouncementType = {
 
 const type = AnnouncementType.FUNDRAISING;
 const activeWiki = 'en.wikipedia.org';
-const startTime = '2017-11-30T16:00:00Z';
+const startTime = '2017-12-06T16:00:00Z';
 const endTime = '2017-12-31T00:00:00Z';
 const idPrefix = 'EN1217';
 const androidDestinationUrl
@@ -16,7 +16,7 @@ const iosDestinationUrl
     = 'https://donate.wikimedia.org/?uselang=en&utm_medium=WikipediaAppFeed&utm_campaign=iOS&utm_source=app_201712_6C_control';
 // TODO: consider removing jshint
 // const imageUrl = undefined; // no image this time
-const buttonLabel = 'Donate today';
+const buttonLabel = 'Donate now';
 const disclaimerHtml
     = 'Privacy disclaimer text: By submitting, you are agreeing to our <a href="https://wikimediafoundation.org/wiki/Donor_policy/en">donor privacy policy</a>';
 
@@ -66,7 +66,9 @@ const buildBaseBodyText = ({ country, currency, average, coffee }) => {
     // replaced %COUNTRY% with ${country}, ...
     /* eslint-disable max-len */
     return `Hi reader in ${country}, it seems you use Wikipedia a lot; I think that’s great and hope you find it useful.
+
 It’s a little awkward to ask, but today we need your help. We depend on donations averaging ${currency}${average}, but fewer than 1% of readers choose to give. If you donate just ${currency}${coffee}, you would help keep Wikipedia thriving for years. That’s right, the price of a cup of coffee is all I ask. Please take a minute to keep Wikipedia growing.
+
 Thank you. — Jimmy Wales, Wikipedia Founder`;
     /* eslint-enable max-len */
 };
