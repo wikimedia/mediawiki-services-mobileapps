abd03a8231c12144b6a3daa7b14b728e5cec11ab
Michael Holloway <mholloway@wikimedia.org>
2017-12-04 13:06:06 -0500

    Hygiene: Fix fixVideoAnchor test
    This test wasn't doing much; a video-like HTML snippet was tested but the
    isVideo check was failing (probably unintentionally). This updates the
    test to do what it appears intended to do.
    
    Change-Id: Ie16de3bfccf2d19db7e98a66b825d9484a4532f7

--- test/lib/transforms/app-transforms-test.js
+++ test/lib/transforms/app-transforms-test.js
@@ -26,14 +26,14 @@ describe('lib:app-transforms', () => {
     it('fixVideoAnchor should transform actual videos', () => {
         const doc = domino.createDocument(`
 <figure typeof="mw:Video/Thumb mw:Placeholder" id="mwBw"><span id="mwCA">
-    <video controls="">
+    <video resource="https://upload.wikimedia.org/wikipedia/commons/9/96/Curiosity%27s_Seven_Minutes_of_Terror.ogv">
         <source src="https://upload.wikimedia.org/wikipedia/commons/9/96/Curiosity%27s_Seven_Minutes_of_Terror.ogv"
         type='video/ogg; codecs="theora, vorbis"' />
     </video>
 </span></figure>`);
         a.fixVideoAnchor(doc);
         const videoThumbImgElements = doc.querySelectorAll('a.app_media');
-        assert.equal(videoThumbImgElements.length, 0, 'Should have marked the video file');
+        assert.equal(videoThumbImgElements.length, 1, 'Should have marked the video file');
     });
 
     it('fixVideoAnchor should apply app_media class to video anchors', () => {
