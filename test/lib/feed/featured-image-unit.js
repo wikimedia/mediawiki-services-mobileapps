26908997fa6e07204af14e5f76d38b0073ac1da6
Michael Holloway <mholloway@wikimedia.org>
2018-01-16 15:25:55 -0500

    Refine description selection
    Simplifies the description picking logic away from its current two-
    step approach to a single step.
    
    Additionally, drops the secondary fallback behavior of returning the
    first available description in the event a description is not
    available in either the preferred language or the configured fallback
    (English), since users are not likely to have any use for a
    description in a totally random language.
    
    Change-Id: Idc86f90674ef1bee4f6bbbb64b8058ed105d97f4

--- test/lib/feed/featured-image-unit.js
+++ test/lib/feed/featured-image-unit.js
@@ -2,27 +2,29 @@
 
 const assert = require('../../utils/assert');
 const featuredImage = require('../../../lib/feed/featured-image'); // module under test
-const pickDescriptionLang = featuredImage.testing.pickDescriptionLang;
+const getDescription = featuredImage.testing.getDescription;
 
 describe('featured-image-unit', () => {
 
-    it('pickDescriptionLang resolves to lang if present', () => {
-        const result = pickDescriptionLang({ 'en': 'foo', 'de': 'bar' }, 'de');
-        assert.deepEqual(result, 'de');
+    it('getDescription returns description for preferred lang if present', () => {
+        const result = getDescription({ 'en':'foo','de':'bar' }, 'de');
+        assert.deepEqual(result.lang, 'de');
+        assert.deepEqual(result.text, 'bar');
     });
 
-    it('pickDescriptionLang falls back to en if lang not present', () => {
-        const result = pickDescriptionLang({ 'en': 'foo', 'de': 'bar' }, 'ja');
-        assert.deepEqual(result, 'en');
+    it('getDescription falls back to en description if preferred lang not present', () => {
+        const result = getDescription({ 'en':'foo','de':'bar' }, 'ja');
+        assert.deepEqual(result.lang, 'en');
+        assert.deepEqual(result.text, 'foo');
     });
 
-    it('pickDescriptionLang returns undefined for non-object input', () => {
-        const result = pickDescriptionLang("<span>lol</span>", 'zh');
+    it('getDescription returns undefined for non-object input', () => {
+        const result = getDescription("<span>lol</span>", 'zh');
         assert.deepEqual(result, undefined);
     });
 
-    it('pickDescriptionLang returns undefined for undefined input', () => {
-        const result = pickDescriptionLang(undefined, 'es');
+    it('getDescription returns undefined for undefined input', () => {
+        const result = getDescription(undefined, 'es');
         assert.deepEqual(result, undefined);
     });
 
