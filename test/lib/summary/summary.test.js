caafd343064b2c79b36ea474dd6262ee1715e2d5
jdlrobson <jdlrobson@gmail.com>
2018-01-16 15:38:33 -0800

    Summaries should not contain coordinates
    Bug: T185050
    Change-Id: Idcb882eeabad8c63c18cf0e94a25ca799899445d

--- test/lib/summary/summary.test.js
+++ test/lib/summary/summary.test.js
@@ -2,8 +2,17 @@
 
 const assert = require('../../utils/assert.js');
 const unit = require('../../../lib/summary').testing;
+const domino = require('domino');
 
 describe('lib:summary', () => {
+    describe('buildExtracts', () => {
+        it('Applies ', () => {
+            const doc = domino.createDocument(`<p><span>
+                <span id="coordinates"><a>Hello</a></span></span></p><p>2</p>`);
+            const extract = unit.buildExtracts(doc, false);
+            assert.deepEqual(extract.extract_html, '<p>2</p>', 'Unneeded markup is stripped.');
+        });
+    });
     describe('getSummaryType', () => {
         it('identifies main page', () => {
             assert.deepEqual(unit.getSummaryType({ "mainpage": true }), 'mainpage');
