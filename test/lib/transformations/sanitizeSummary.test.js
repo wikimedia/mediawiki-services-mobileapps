2bb0fd71678a7448fe937cbbbfbfbab88c1cdd5e
BearND <bsitzmann@wikimedia.org>
2018-01-31 13:52:14 -0700

    Summary: tighter regexs for style checking
    Don't allow any colons and slashes in ANY_REGEX.
    Reduced number of allowed characters to 80 or sometimes 40 (for the
    colors).
    Also dropped '-' in SINGLE_STRING_REGEX since I don't see any color
    names in the CSS spec right now.
    Added more tests and explanatory messages.
    
    Change-Id: I3d779668a7687bdc219e5861016640c52a63c5a5

--- test/lib/transformations/sanitizeSummary.test.js
+++ test/lib/transformations/sanitizeSummary.test.js
@@ -8,31 +8,38 @@ describe('lib:sanitizeSummary', () => {
     const makeLongString = () => {
         const template = 'abcdefghij';
         let result = '';
-        for (let i = 0; i < 20; i++) {
+        for (let i = 0; i < 8; i++) {
             result += template;
         }
         return result;
     };
-    const longString = makeLongString();
-    const tooLongString = `${longString}a`;
+    const eightyChars = makeLongString();
+    const eightyOneChars = `${eightyChars}a`;
 
     describe('regular expressions', () => {
         it('ANY_REGEX matches', () => {
             assert.ok(regex.ANY_REGEX.test('a'));
-            assert.ok(regex.ANY_REGEX.test(longString));
+            assert.ok(regex.ANY_REGEX.test(eightyChars));
         });
         it('ANY_REGEX does not match', () => {
             assert.ok(!regex.ANY_REGEX.test(''));
-            assert.ok(!regex.ANY_REGEX.test(tooLongString));
+            assert.ok(!regex.ANY_REGEX.test('foo:bar'));
+            assert.ok(!regex.ANY_REGEX.test(eightyOneChars));
         });
 
         it('DECIMAL_REGEX matches', () => {
+            assert.ok(regex.DECIMAL_REGEX.test('1'));
+            assert.ok(regex.DECIMAL_REGEX.test('0.1'));
+            assert.ok(regex.DECIMAL_REGEX.test('.1'));
             assert.ok(regex.DECIMAL_REGEX.test('1.2'));
             assert.ok(regex.DECIMAL_REGEX.test('-1.2'));
         });
         it('DECIMAL_REGEX does not match', () => {
-            assert.ok(!regex.DECIMAL_REGEX.test('a'));
-            assert.ok(!regex.DECIMAL_REGEX.test(' '));
+            // not sure about the next one, mainly documenting for now
+            assert.ok(!regex.DECIMAL_REGEX.test('1.'), 'needs a number after .');
+            assert.ok(!regex.DECIMAL_REGEX.test('.'), 'needs a number after .');
+            assert.ok(!regex.DECIMAL_REGEX.test('a1'), 'no letter allowed');
+            assert.ok(!regex.DECIMAL_REGEX.test(' '), 'no space allowed');
         });
 
         it('CSS_SIZE_REGEX matches', () => {
@@ -41,24 +48,24 @@ describe('lib:sanitizeSummary', () => {
             assert.ok(regex.CSS_SIZE_REGEX.test('30%'));
         });
         it('CSS_SIZE_REGEX does not match', () => {
-            assert.ok(!regex.CSS_SIZE_REGEX.test('a'));
-            assert.ok(!regex.CSS_SIZE_REGEX.test(' '));
-            assert.ok(!regex.CSS_SIZE_REGEX.test('1'));
-            assert.ok(!regex.CSS_SIZE_REGEX.test('20px and more'));
+            assert.ok(!regex.CSS_SIZE_REGEX.test('1'), 'must have a unit');
+            assert.ok(!regex.CSS_SIZE_REGEX.test('a1px'), 'no letters');
+            assert.ok(!regex.CSS_SIZE_REGEX.test('20px and more'), 'no space');
         });
 
         it('SINGLE_STRING_REGEX matches', () => {
             assert.ok(regex.SINGLE_STRING_REGEX.test('green'));
-            assert.ok(regex.SINGLE_STRING_REGEX.test('foo-bar')); // TODO: disallow
-            assert.ok(regex.SINGLE_STRING_REGEX.test('-20px')); // TODO: disallow
-            assert.ok(regex.SINGLE_STRING_REGEX.test(longString));
+            assert.ok(regex.SINGLE_STRING_REGEX.test(eightyChars));
         });
         it('SINGLE_STRING_REGEX does not match', () => {
-            assert.ok(!regex.SINGLE_STRING_REGEX.test(' '));
-            assert.ok(!regex.SINGLE_STRING_REGEX.test('green and more'));
-            assert.ok(!regex.SINGLE_STRING_REGEX.test('30%'));
-            assert.ok(!regex.SINGLE_STRING_REGEX.test('foo:'));
-            assert.ok(!regex.SINGLE_STRING_REGEX.test(tooLongString));
+            assert.ok(!regex.SINGLE_STRING_REGEX.test(' '), 'no space allowed');
+            assert.ok(!regex.SINGLE_STRING_REGEX.test('green and more'), 'no space allowed');
+            assert.ok(!regex.SINGLE_STRING_REGEX.test('30%'), 'no % allowed');
+            assert.ok(!regex.SINGLE_STRING_REGEX.test('foo:'), 'no : allowed');
+            assert.ok(!regex.SINGLE_STRING_REGEX.test('foo-bar'), 'no - allowed');
+            assert.ok(!regex.SINGLE_STRING_REGEX.test('-20px'), 'no - allowed');
+            assert.ok(!regex.SINGLE_STRING_REGEX.test('fö'), 'no non-ASCII characters allowed');
+            assert.ok(!regex.SINGLE_STRING_REGEX.test(eightyOneChars), 'string too long');
         });
 
         it('HEX_REGEX matches', () => {
@@ -67,10 +74,10 @@ describe('lib:sanitizeSummary', () => {
             assert.ok(regex.HEX_REGEX.test('#1a2B3c4d'));
         });
         it('HEX_REGEX does not match', () => {
-            assert.ok(!regex.HEX_REGEX.test('1ac'));
-            assert.ok(!regex.HEX_REGEX.test('#1ac and more'));
-            assert.ok(!regex.HEX_REGEX.test('30%'));
-            assert.ok(!regex.HEX_REGEX.test('foo:'));
+            assert.ok(!regex.HEX_REGEX.test('1ac'), 'must start with #');
+            assert.ok(!regex.HEX_REGEX.test('#1ac and more'), 'no space allowed');
+            assert.ok(!regex.HEX_REGEX.test('#30%'), 'no % allowed');
+            assert.ok(!regex.HEX_REGEX.test('#foo:'), 'no : allowed');
         });
 
         it('RGB_REGEX matches', () => {
@@ -82,9 +89,10 @@ describe('lib:sanitizeSummary', () => {
             assert.ok(regex.RGB_REGEX.test('rgba(34.6 12 64 / 30%)'));
         });
         it('RGB_REGEX does not match', () => {
-            assert.ok(!regex.RGB_REGEX.test('not starting with rgb'));
-            assert.ok(!regex.RGB_REGEX.test('rgb()'));
-            assert.ok(!regex.RGB_REGEX.test('rgb(34, 12, 64, 0.6) and more'));
+            assert.ok(!regex.RGB_REGEX.test('foo'), 'must start with rgb( or rgba(');
+            assert.ok(!regex.RGB_REGEX.test('rgb()'), 'rgb/a needs some arguments');
+            assert.ok(!regex.RGB_REGEX.test('rgb(134, 112, 164, 0.611) foo'), 'must end with )');
+            assert.ok(!regex.RGB_REGEX.test('rgb(foo:)'), 'no colon inside ()');
         });
 
         it('HSL_REGEX matches', () => {
@@ -101,10 +109,10 @@ describe('lib:sanitizeSummary', () => {
             assert.ok(regex.HSL_REGEX.test('hsla(240 100% 50% / 5%)'));
         });
         it('HSL_REGEX does not match', () => {
-            assert.ok(!regex.HSL_REGEX.test('1ac'));
-            assert.ok(!regex.HSL_REGEX.test('hsl(34, 12, 64, 0.6) and more'));
-            assert.ok(!regex.HSL_REGEX.test('30%'));
-            assert.ok(!regex.HSL_REGEX.test('foo:'));
+            assert.ok(!regex.HSL_REGEX.test('foo'), 'must start with hsl( or hsla(');
+            assert.ok(!regex.RGB_REGEX.test('hsl()'), 'hsl/a needs some arguments');
+            assert.ok(!regex.HSL_REGEX.test('hsl(34, 12, 64, 0.6) foo'), 'must end with )');
+            assert.ok(!regex.HSL_REGEX.test('hsl(foo:)'), 'no colon inside ()');
         });
     });
 });
