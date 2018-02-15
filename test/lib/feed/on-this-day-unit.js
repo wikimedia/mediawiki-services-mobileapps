83e55a4ae65f377269c479634c5570c36eeefd5d
BearND <bsitzmann@wikimedia.org>
2018-02-14 21:07:28 -0700

    Hygiene: move live on-this-day tests out of lib folder
    These are not unit tests and require a running server.
    The tests are moved as is.
    Only some formatting changes + changed on function declaration to
    an arrow function to make eslint happy.
    
    Change-Id: Ifc7dc8a33a3cf824b1c4059064c1bb42f4727a39

--- test/lib/feed/on-this-day-unit.js
+++ test/lib/feed/on-this-day-unit.js
@@ -1,25 +1,13 @@
 'use strict';
 
-const server = require('../../utils/server.js');
 const onThisDay = require('../../../lib/feed/on-this-day.js');
 const assert = require('../../utils/assert.js');
 const domino = require('domino');
-const preq = require('preq');
-const headers = require('../../utils/headers');
 const fs = require('fs');
 const path = require('path');
 const rbTemplate = require('../../utils/testUtil').rbTemplate;
-
 const onThisDayLangs = require('../../../lib/feed/on-this-day.languages.js');
 const languages = onThisDayLangs.languages;
-const eventTypes = [
-    'all',
-    'selected',
-    'births',
-    'deaths',
-    'events',
-    'holidays'
-];
 
 // UTILITY
 
@@ -87,11 +75,7 @@ const NON_EVENT_LIST_ELEMENT = MOCK_EVENT_LIST_ELEMENTS[5];
 
 // TESTS
 
-describe('onthisday', function() {
-
-    this.timeout(20000); // eslint-disable-line no-invalid-this
-
-    before(() => { return server.start(); });
+describe('onthisday-unit', () => {
 
     // TEST PAGE TITLE GENERATION
 
@@ -278,58 +262,6 @@ describe('onthisday', function() {
 
     it('wmfEventFromListElement should return null for elements not describing events', () => {
         assert.ok(onThisDay.wmfEventFromListElement(NON_EVENT_LIST_ELEMENT, 'en') === null);
-    });
-
-    // LIVE TEST ENDPOINT INTERNALS PRODUCE AT LEAST SOME RESULTS FOR A GIVEN DAY.
-    // DO NOT TEST FOR EXACT RESULT COUNT - THESE CHANGE AS PAGES ARE EDITED.
-    // INSTEAD TEST THAT AT LEAST SOME RESULTS ARE RETURNED.
-
-    function january30uriForEndpointName(endpointName, lang = 'en') {
-        return `${server.config.uri}${lang}.wikipedia.org/v1/feed/onthisday/${endpointName}/01/30/`;
-    }
-    function getJanuary30ResponseForEndpointName(endpointName, lang) {
-        return preq.get(january30uriForEndpointName(endpointName, lang));
-    }
-    function verifyNonZeroEndpointResults(response, endpointName) {
-        assert.deepEqual(response.status, 200);
-        assert.ok(response.body[endpointName].length > 0,
-            `${endpointName} should have fetched some results`);
-    }
-    function fetchAndVerifyNonZeroResultsForEndpointName(endpointName, lang) {
-        return getJanuary30ResponseForEndpointName(endpointName, lang)
-         .then((response) => {
-             verifyNonZeroEndpointResults(response, endpointName);
-         });
-    }
-
-    for (const type of eventTypes) {
-        it(`${type}: should respond to GET req w/expected headers`, () => {
-            headers.checkHeaders(january30uriForEndpointName(type));
-        });
-
-        it(`${type}: unsupported language throws 501`, () => {
-            return getJanuary30ResponseForEndpointName(type, 'nl')
-            .catch((error) => {
-                assert.equal(error.status, 501);
-            });
-        });
-
-        if (type !== 'all') {
-            it(`${type}: fetches some results`, () => {
-                return fetchAndVerifyNonZeroResultsForEndpointName(type);
-            });
-        }
-    }
-
-    it('"all" fetches some results for births, deaths, events, holidays and selected', () => {
-        return getJanuary30ResponseForEndpointName('all')
-        .then((response) => {
-            assert.ok(response.body.births.length > 0, 'ALL should return some births');
-            assert.ok(response.body.deaths.length > 0, 'ALL should return some deaths');
-            assert.ok(response.body.events.length > 0, 'ALL should return some events');
-            assert.ok(response.body.holidays.length > 0, 'ALL should return some holidays');
-            assert.ok(response.body.selected.length > 0, 'ALL should return some selected');
-        });
     });
 
     it('eventsForYearListElements returns a WMFEvent for only year list elements', () => {
