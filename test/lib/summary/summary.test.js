ba6c24a7fb4540e85f715e858cb411c66e793ed4
BearND <bsitzmann@wikimedia.org>
2018-02-23 00:09:34 -0700

    Summary: avoid selecting error spans for lead paragraph
    Something like "Lua error: not enough memory." doesn't make for a good
    summary.
    Removes any span which has a class with the substring 'error'.
    
    Bug: 187936
    Change-Id: I8bb3824a53a5a2b627cbec803f709f337c54df0f

--- test/lib/summary/summary.test.js
+++ test/lib/summary/summary.test.js
@@ -6,13 +6,24 @@ const domino = require('domino');
 
 describe('lib:summary', () => {
     describe('buildExtracts', () => {
-        it('Applies stripUnneededMarkup', () => {
-            const doc = domino.createDocument(`<p><span>
-                <span id="coordinates"><a>Hello</a></span></span></p><p>2</p>`);
+        function test(inputString, expected, message) {
+            const doc = domino.createDocument(inputString);
             const extract = unit.buildExtracts(doc, { ns: 0, contentmodel: 'wikitext' });
-            assert.deepEqual(extract.extract_html, '<p>2</p>', 'Unneeded markup is stripped.');
+            assert.deepEqual(extract.extract_html, expected, message);
+        }
+        it('Applies stripUnneededMarkup', () => {
+            test('<p><span><span id="coordinates"><a>Hello</a></span></span></p><p>2</p>',
+                '<p>2</p>', 'Unneeded markup is stripped.');
+        });
+        // https://bg.wikipedia.org/api/rest_v1/page/html/%D0%92%D1%82%D0%BE%D1%80%D0%B0_%D1%81%D0%B2%D0%B5%D1%82%D0%BE%D0%B2%D0%BD%D0%B0_%D0%B2%D0%BE%D0%B9%D0%BD%D0%B0/8391615
+        it('Don\'t select scribunto errors.', () => {
+            test(
+                `<p><strong><span class="scribunto-error">Грешка в Lua: not enough memory.
+</span></strong></p>`,
+                '');
         });
     });
+
     describe('getSummaryType', () => {
         it('identifies main page', () => {
             assert.deepEqual(unit.getSummaryType({ "mainpage": true }), 'mainpage');
