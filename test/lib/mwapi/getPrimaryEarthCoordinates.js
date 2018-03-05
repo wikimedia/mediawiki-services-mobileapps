86a1f3c03f39cc3a49bc101d5dea79dc384fa456
Michael Holloway <mholloway@wikimedia.org>
2018-03-05 11:02:45 -0500

    Hygiene: Stop checking coordinates for legacy MW API booleans
    Now that GeoData is updated to respond with boolean values for JSON
    format 2, there's no need to check for legacy empty string booleans.
    
    Also updates the unit tests for getPrimaryEarthCoordinates to follow
    the preferred pattern.
    
    Bug: T179947
    Change-Id: Ic3c920a065127e09ff47fb14cc0c4ad3254dfa3a

--- test/lib/mwapi/getPrimaryEarthCoordinates.js
+++ test/lib/mwapi/getPrimaryEarthCoordinates.js
@@ -1,44 +1,36 @@
 'use strict';
 
 const assert = require('../../utils/assert.js');
-const getGeo = require('../../../lib/mwapi').getPrimaryEarthCoordinates;
+const getPrimaryEarthCoordinates = require('../../../lib/mwapi').getPrimaryEarthCoordinates;
+
+const test = (coords, expected) => {
+    const result = getPrimaryEarthCoordinates(coords);
+    if (result) {
+        assert.deepEqual(result.latitude, expected.latitude);
+        assert.deepEqual(result.longitude, expected.longitude);
+    } else {
+        assert.deepEqual(result, expected);
+    }
+};
 
 describe('lib:mwapi:getPrimaryEarthCoordinates', () => {
-    it('gets only primary earth coordinates', () => {
-        const testCases = [
-            [
-                // primary earth coordinates
-                [{ lat: 0, lon: 0, primary: '', globe: 'earth' }],
-                { latitude: 0, longitude: 0 }
-            ],
-            [
-                // primary earth coordinates (multiple choices)
-                [
-                    { lat: 0, lon: 0, globe: 'earth' },
-                    { lat: 1, lon: 1, primary: '', globe: 'earth' }
-                ],
-                { latitude: 1, longitude: 1 }
-            ],
-            [
-                // not primary
-                [{ lat: 0, lon: 0, globe: 'earth' }],
-                undefined
-            ],
-            [
-                // wrong globe
-                [{ lat: 0, lon: 0, primary: '', globe: 'moon' }],
-                undefined
-            ],
-        ];
-        testCases.forEach((test) => {
-            const result = getGeo(test[0]);
-            const expected = test[1];
-            if (result) {
-                assert.deepEqual(result.latitude, expected.latitude);
-                assert.deepEqual(result.longitude, expected.longitude);
-            } else {
-                assert.deepEqual(result, expected);
-            }
-        });
+    it('gets primary earth coordinates (single coordinate input)', () => {
+        test([ { lat: 0, lon: 0, primary: true, globe: 'earth' } ], { latitude: 0, longitude: 0 });
+    });
+
+    it('gets primary earth coordinates (multiple coordinate input)', () => {
+        test([
+            { lat: 0, lon: 0, primary: false, globe: 'earth' },
+            { lat: 1, lon: 1, primary: true, globe: 'earth' }
+        ],
+        { latitude: 1, longitude: 1 });
+    });
+
+    it('secondary coordinates are ignored', () => {
+        test([ { lat: 0, lon: 0, primary: false, globe: 'earth' } ], undefined);
+    });
+
+    it('non-earth coordinates are ignored', () => {
+        test([ { lat: 0, lon: 0, primary: true, globe: 'moon' } ], undefined);
     });
 });
