5eebea434c62a998114343c78872b45a06666b30
Michael Holloway <mholloway@wikimedia.org>
2018-03-07 13:13:16 -0500

    Media endpoint response tweaks
    Final tweaks to the media response requested by Corey.
    
    * Return license type and url as a single object under 'license'
    * Parse structured artist info from extmetadata HTML
    * Change 'source'->'url' in video objects under 'sources'
    * Give audio files the 'generic' type if not 'pronunciation' or 'spoken'
    
    Bug: T177430
    Change-Id: Ia6f5d36a4b025848e144d46159b137ac970cf5b5

--- test/lib/media/media-test-metadata.js
+++ test/lib/media/media-test-metadata.js
@@ -2,6 +2,7 @@
 
 const assert = require('../../utils/assert');
 const media = require('../../../lib/media');
+const getStructuredArtistInfo = media.testing.getStructuredArtistInfo;
 
 const imageWithCaption =
     '<figure typeof="mw:Image">' +
@@ -63,7 +64,7 @@ describe('lib:media metadata is correctly parsed from HTML', () => {
     it('all expected derivative properties are present', () => {
         const result = media.getMediaItemInfoFromPage(videoWithDerivative)[0];
         const derivative = result.sources[0];
-        assert.deepEqual(derivative.source, 'https://example.com/Foo.ogv');
+        assert.deepEqual(derivative.url, 'https://example.com/Foo.ogv');
         assert.deepEqual(derivative.mime, 'video/ogg');
         assert.deepEqual(derivative.codecs, [ "theora", "vorbis" ]);
         assert.deepEqual(derivative.name, 'Foo');
@@ -89,4 +90,43 @@ describe('lib:media metadata is correctly parsed from HTML', () => {
         assert.deepEqual(result.section_id, 0);
     });
 
+});
+
+describe('lib:media parse structured artist info', () => {
+    it('all info is parsed from common HTML structure', () => {
+        const html = '<a href="//commons.wikimedia.org/wiki/User:Foo" title="User:Foo">Foo</a>';
+        const result = getStructuredArtistInfo(html);
+        assert.deepEqual(result.html, html);
+        assert.deepEqual(result.name, 'Foo');
+        assert.deepEqual(result.user_page, 'https://commons.wikimedia.org/wiki/User:Foo');
+    });
+
+    it('\'html\' and \'name\' fields are returned from plain text input', () => {
+        const html = 'Foo';
+        const result = getStructuredArtistInfo(html);
+        assert.deepEqual(result.html, html);
+        assert.deepEqual(result.name, html);
+        assert.deepEqual(result.user_page, undefined);
+    });
+
+    it('only html returned for site other than Commons', () => {
+        const html = '<a href="//example.com/wiki/User:Foo" title="User:Foo">Foo</a>';
+        const result = getStructuredArtistInfo(html);
+        assert.deepEqual(result.html, html);
+        assert.deepEqual(result.name, undefined);
+        assert.deepEqual(result.user_page, undefined);
+    });
+
+    it('only html returned if non-namespace portion of the title !== html.textContent', () => {
+        const html = '<a href="//commons.wikimedia.org/wiki/User:Foo" title="User:Foo">Bar</a>';
+        const result = getStructuredArtistInfo(html);
+        assert.deepEqual(result.html, html);
+        assert.deepEqual(result.name, undefined);
+        assert.deepEqual(result.user_page, undefined);
+    });
+
+    it('undefined result if input is an empty string', () => {
+        const result = getStructuredArtistInfo('');
+        assert.deepEqual(result, undefined);
+    });
 });
