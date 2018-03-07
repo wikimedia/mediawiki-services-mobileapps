bfea9cce5e4999d46f0c7ef5311c042b2b01519c
Michael Holloway <mholloway@wikimedia.org>
2018-03-07 08:47:55 -0500

    Metadata endpoint response tweaks
    Final tweaks to the metadata response requested by Corey.
    
    * Remove 'toc' prefixes from toc entry properties
    * Remove flags from TOC output and do not honor them
    * Add titles dictionary and summary link to categories
    * Add titles dictionary and summary link to language links
    * Update property name langlinks->language_links
    * Stop reporting coordinates since primary (Earth) coords are already in
      the summary
    * Remove 'spoken' property since it's in the media endpoint
    
    Bug: T177428
    Change-Id: I17605269758248f0448469f432837e01e4cf55af

--- test/lib/metadata/buildTableOfContents-unit.js
+++ test/lib/metadata/buildTableOfContents-unit.js
@@ -1,9 +1,6 @@
 'use strict';
 
-const domino = require('domino');
 const assert = require('../../utils/assert');
-const getTocLimit = require('../../../lib/metadata').testing.getTocLimit;
-const getTocFlags = require('../../../lib/metadata').testing.getTocFlags;
 const buildTocEntries = require('../../../lib/metadata').testing.buildTocEntries;
 
 const sections = [
@@ -33,17 +30,6 @@ const deepSections = [
     { id: 10, toclevel: 10, line: "Foo", anchor: "Foo" }
 ];
 
-function getTocLimitDoc(classList) {
-    return domino.createDocument(`<div class="${classList.join(' ')}"></div>`);
-}
-
-function assertDeepSectionsTocLimit(lim, length, last) {
-    const flags = getTocFlags(getTocLimitDoc([ `toclimit-${lim}` ]), { pageprops: {} });
-    const result = buildTocEntries(deepSections, flags);
-    assert.deepEqual(result.length, length);
-    assert.deepEqual(result[length - 1].tocnumber, last);
-}
-
 describe('lib:metadata', () => {
     it('should exclude lead section and non-displayable or pseudo-sections', () => {
         const result = buildTocEntries(sections, {});
@@ -57,61 +43,10 @@ describe('lib:metadata', () => {
 
     it('toc numbers should reflect the toc level hierarchy', () => {
         const expected = ['1', '1.1', '1.1.1', '1.2', '1.2.1', '1.3', '2'];
-        const result = buildTocEntries(sections, {}).map(i => i.tocnumber);
+        const result = buildTocEntries(sections, {}).map(i => i.number);
         assert.deepEqual(result.length, expected.length);
         for (let i = 0; i < expected.length; i++) {
             assert.deepEqual(result[i], expected[i]);
         }
-    });
-
-    it('toclimit div limits TOC depth', () => {
-        assertDeepSectionsTocLimit(2, 1, '1');
-        assertDeepSectionsTocLimit(3, 2, '1.1');
-        assertDeepSectionsTocLimit(4, 3, '1.1.1');
-        assertDeepSectionsTocLimit(5, 4, '1.1.1.1');
-        assertDeepSectionsTocLimit(6, 5, '1.1.1.1.1');
-    });
-
-    it('getTocFlags gets notoc pageprop when present', () => {
-        const html = '<meta property="mw:PageProp/notoc">';
-        const result = getTocFlags(domino.createDocument(html));
-        assert.deepEqual(result, { notoc: true });
-    });
-
-    it('getTocFlags gets forcetoc pageprop when present', () => {
-        const html = '<meta property="mw:PageProp/forcetoc">';
-        const result = getTocFlags(domino.createDocument(html));
-        assert.deepEqual(result, { forcetoc: true });
-    });
-
-    it('forcetoc overrides notoc', () => {
-        const html = `<meta property="mw:PageProp/notoc"><meta property="mw:PageProp/forcetoc">`;
-        const result = getTocFlags(domino.createDocument(html));
-        assert.deepEqual(result, { forcetoc: true });
-    });
-
-    it('no toclimit if notoc=true', () => {
-        const html = `<meta property="mw:PageProp/notoc"><div class="toclimit-5"></div>`;
-        const result = getTocFlags(domino.createDocument(html));
-        assert.deepEqual(result, { notoc: true });
-    });
-
-    it('toclimit is present if notoc is overridden by forcetoc', () => {
-        const html = `<meta property="mw:PageProp/notoc"><meta property="mw:PageProp/forcetoc">
-            <div class="toclimit-5"></div>`;
-        const result = getTocFlags(domino.createDocument(html));
-        assert.deepEqual(result, { forcetoc: true, toclimit: 5 });
-    });
-
-    it('getTocLimit handles multi-digit toclimit values', () => {
-        const result = getTocLimit(getTocLimitDoc([ 'toclimit-10' ]));
-        const result2 = getTocLimit(getTocLimitDoc([ 'toclimit-100' ]));
-        assert.deepEqual(result, 10);
-        assert.deepEqual(result2, 100);
-    });
-
-    it('getTocLimit handles other classes in the toclimit div\'s classlist', () => {
-        const result = getTocLimit(getTocLimitDoc([ 'foo', 'toclimit-10', 'bar' ]));
-        assert.deepEqual(result, 10);
     });
 });
