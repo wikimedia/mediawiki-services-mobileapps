ae42e915623c7a98fe2863d7012d3243e36a67eb
Michael Holloway <mholloway@wikimedia.org>
2018-03-09 14:47:34 -0500

    Hygiene: Refine thumb URL manipulation tests
    Abstracts out the filenames in order to make the test cases more focused
    on the logic under test.
    
    Change-Id: I2367edddbf692eed4aae635c2702022c7b813ee2

--- test/lib/mwapi/image-test.js
+++ test/lib/mwapi/image-test.js
@@ -6,53 +6,53 @@ const scale = require('../../../lib/mwapi').scaledImageUrl;
 
 const path = 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a0';
 
+// https://upload.wikimedia.org/wikipedia/commons/thumb/a/a0/Foo.jpg/2000px-Foo.jpg
+const thumb = (size, file = 'Foo.jpg', prefix = '') => `${path}/${file}/${prefix}${size}px-${file}`;
+
 describe('lib:mwapi', () => {
     it('image URLs are rewritten if URL contains a size and the desired width is smaller', () => {
-        const prefix = 'https://upload.wikimedia.org/wikipedia/commons';
-        assert.deepEqual(scale(`${prefix}/thumb/0/0b/Cat_poster_1.jpg/720px-Cat_poster_1.jpg`, 320),
-            `${prefix}/thumb/0/0b/Cat_poster_1.jpg/320px-Cat_poster_1.jpg`);
-        assert.deepEqual(scale(`${prefix}/thumb/0/0b/Cat_poster_1.jpg/120px-Cat_poster_1.jpg`, 320),
-            `${prefix}/thumb/0/0b/Cat_poster_1.jpg/120px-Cat_poster_1.jpg`);
-        assert.deepEqual(scale(`${prefix}/9/96/Vasskertentrance.jpg`, 320),
-            `${prefix}/9/96/Vasskertentrance.jpg`);
+        const notAThumb = 'https://upload.wikimedia.org/wikipedia/commons/9/96/Vasskertentrance.jpg';
+        assert.deepEqual(scale(thumb(720), 320), thumb(320));
+        assert.deepEqual(scale(thumb(120), 320), thumb(120));
+        assert.deepEqual(scale(notAThumb, 320), notAThumb);
     });
 });
 
 describe('lib:mwapi buildLeadImageUrls', () => {
 
     it('2000px thumb should be resized for all sizes', () => {
-        assert.deepEqual(buildUrls(`${path}/Sun_in_February.jpg/2000px-Sun_in_February.jpg`), {
-            320: `${path}/Sun_in_February.jpg/320px-Sun_in_February.jpg`,
-            640: `${path}/Sun_in_February.jpg/640px-Sun_in_February.jpg`,
-            800: `${path}/Sun_in_February.jpg/800px-Sun_in_February.jpg`,
-            1024: `${path}/Sun_in_February.jpg/1024px-Sun_in_February.jpg`
+        assert.deepEqual(buildUrls(thumb(2000)), {
+            320: thumb(320),
+            640: thumb(640),
+            800: thumb(800),
+            1024: thumb(1024)
         });
     });
 
     it('555px thumb should return 320 and 555 for rest', () => {
-        assert.deepEqual(buildUrls(`${path}/Sun_in_February.jpg/555px-Sun_in_February.jpg`), {
-            320: `${path}/Sun_in_February.jpg/320px-Sun_in_February.jpg`,
-            640: `${path}/Sun_in_February.jpg/555px-Sun_in_February.jpg`,
-            800: `${path}/Sun_in_February.jpg/555px-Sun_in_February.jpg`,
-            1024: `${path}/Sun_in_February.jpg/555px-Sun_in_February.jpg`
+        assert.deepEqual(buildUrls(thumb(555)), {
+            320: thumb(320),
+            640: thumb(555),
+            800: thumb(555),
+            1024: thumb(555)
         });
     });
 
     it('750px thumb should return 320, 640, and 750 for rest', () => {
-        assert.deepEqual(buildUrls(`${path}/Sun_in_February.jpg/750px-Sun_in_February.jpg`), {
-            320: `${path}/Sun_in_February.jpg/320px-Sun_in_February.jpg`,
-            640: `${path}/Sun_in_February.jpg/640px-Sun_in_February.jpg`,
-            800: `${path}/Sun_in_February.jpg/750px-Sun_in_February.jpg`,
-            1024: `${path}/Sun_in_February.jpg/750px-Sun_in_February.jpg`
+        assert.deepEqual(buildUrls(thumb(750)), {
+            320: thumb(320),
+            640: thumb(640),
+            800: thumb(750),
+            1024: thumb(750)
         });
     });
 
     it('200px thumb should return size 200 for all URLs', () => {
-        assert.deepEqual(buildUrls(`${path}/Sun_in_February.jpg/200px-Sun_in_February.jpg`), {
-            320: `${path}/Sun_in_February.jpg/200px-Sun_in_February.jpg`,
-            640: `${path}/Sun_in_February.jpg/200px-Sun_in_February.jpg`,
-            800: `${path}/Sun_in_February.jpg/200px-Sun_in_February.jpg`,
-            1024: `${path}/Sun_in_February.jpg/200px-Sun_in_February.jpg`
+        assert.deepEqual(buildUrls(thumb(200)), {
+            320: thumb(200),
+            640: thumb(200),
+            800: thumb(200),
+            1024: thumb(200)
         });
     });
 
@@ -76,33 +76,36 @@ describe('lib:mwapi buildLeadImageUrls', () => {
         });
     });
 
+    // https://upload.wikimedia.org/wikipedia/commons/thumb/a/a0/9999px-Foo.jpg/2000px-9999px-Foo.jpg
     it('should create thumb URLs correctly if size regex pattern is in original filename', () => {
-        const file = `${path}/9999px-Foo.jpg/2000px-9999px-Foo.jpg`;
+        const file = thumb(2000, '9999px-Foo.jpg');
         assert.deepEqual(buildUrls(file), {
-            320: `${path}/9999px-Foo.jpg/320px-9999px-Foo.jpg`,
-            640: `${path}/9999px-Foo.jpg/640px-9999px-Foo.jpg`,
-            800: `${path}/9999px-Foo.jpg/800px-9999px-Foo.jpg`,
-            1024: `${path}/9999px-Foo.jpg/1024px-9999px-Foo.jpg`
+            320: thumb(320, '9999px-Foo.jpg'),
+            640: thumb(640, '9999px-Foo.jpg'),
+            800: thumb(800, '9999px-Foo.jpg'),
+            1024: thumb(1024, '9999px-Foo.jpg')
         });
     });
 
+    // https://upload.wikimedia.org/wikipedia/commons/thumb/a/a0/Foo.jpg/qlow-2000px-Foo.jpg
     it('should handle edge case thumb filename patterns', () => {
-        const file = `${path}/Foo.jpg/qlow-2000px-Foo.jpg`;
+        const file = thumb(2000, 'Foo.jpg', 'qlow-');
         assert.deepEqual(buildUrls(file), {
-            320: `${path}/Foo.jpg/qlow-320px-Foo.jpg`,
-            640: `${path}/Foo.jpg/qlow-640px-Foo.jpg`,
-            800: `${path}/Foo.jpg/qlow-800px-Foo.jpg`,
-            1024: `${path}/Foo.jpg/qlow-1024px-Foo.jpg`
+            320: thumb(320, 'Foo.jpg', 'qlow-'),
+            640: thumb(640, 'Foo.jpg', 'qlow-'),
+            800: thumb(800, 'Foo.jpg', 'qlow-'),
+            1024: thumb(1024, 'Foo.jpg', 'qlow-')
         });
     });
 
+    // https://upload.wikimedia.org/wikipedia/commons/thumb/a/a0/9999px-Foo.jpg/qlow-2000px-9999px-Foo.jpg
     it('should handle edge case thumb filename patterns with size regex in original name', () => {
-        const file = `${path}/9999px-Foo.jpg/qlow-2000px-9999px-Foo.jpg`;
+        const file = thumb(2000, '9999px-Foo.jpg', 'qlow-');
         assert.deepEqual(buildUrls(file), {
-            320: `${path}/9999px-Foo.jpg/qlow-320px-9999px-Foo.jpg`,
-            640: `${path}/9999px-Foo.jpg/qlow-640px-9999px-Foo.jpg`,
-            800: `${path}/9999px-Foo.jpg/qlow-800px-9999px-Foo.jpg`,
-            1024: `${path}/9999px-Foo.jpg/qlow-1024px-9999px-Foo.jpg`
+            320: thumb(320, '9999px-Foo.jpg', 'qlow-'),
+            640: thumb(640, '9999px-Foo.jpg', 'qlow-'),
+            800: thumb(800, '9999px-Foo.jpg', 'qlow-'),
+            1024: thumb(1024, '9999px-Foo.jpg', 'qlow-')
         });
     });
 });
