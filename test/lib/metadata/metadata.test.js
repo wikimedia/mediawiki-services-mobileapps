8fb2cbac80d571fbb2abea1cb89e6b336417da0e
Michael Holloway <mholloway@wikimedia.org>
2018-03-20 14:07:12 -0400

    Fix: Check for undefined inputs when processing metadata
    Bug: T190190
    Change-Id: I2e06d0ddca68a747e5074eff5785ce3769c20217

---
+++ test/lib/metadata/metadata.test.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const lib = require('../../../lib/metadata').testing;
+const assert = require('../../utils/assert');
+
+describe('lib:metadata', () => {
+
+    it('augmentLangLinks handles undefined langlinks', () => {
+        assert.doesNotThrow(() => lib.augmentLangLinks());
+    });
+
+    it('augmentCategores handles undefined categories', () => {
+        assert.doesNotThrow(() => lib.augmentCategories());
+    });
+
+});
