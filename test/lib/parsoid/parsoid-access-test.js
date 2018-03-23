a74787c7d7c098edb99a73e5c91abf9978ca9c25
Michael Holloway <mholloway@wikimedia.org>
2018-03-23 18:04:12 -0400

    Handle weak etags
    The REST API has begun emitting weak etags, so we need to handle them.
    
    Side fix (to keep tests passing): Update Kosice diff test fixture to
    handle a renamed file.
    
    https://commons.wikimedia.org/w/index.php?title=File%3ANew_Orthodox_Synagogue%2C_Ko%C5%A1ice.jpg&type=revision&diff=293613150&oldid=146202258
    
    Change-Id: Ia320046dcb916623e5b27aee10933410c76c40de

--- test/lib/parsoid/parsoid-access-test.js
+++ test/lib/parsoid/parsoid-access-test.js
@@ -1,44 +1,85 @@
 'use strict';
 
-const assert = require('../../utils/assert.js');
 const domino = require('domino');
 const parsoid = require('../../../lib/parsoid-access');
+const assert = require('../../utils/assert');
 
 const headHtml1
     = `<html><head>
         <base href="//en.wikipedia.org/wiki/"/>
         <link rel="dc:isVersionOf" href="//en.wikipedia.org/wiki/Hope_(painting)"/>
     </head></html>`;
+
 const headHtml2
     = `<html>
         <head>
             <base href="//test.wikipedia.org/wiki/"/>
-            <link rel="dc:isVersionOf" 
+            <link rel="dc:isVersionOf"
                   href="//test.wikipedia.org/wiki/User%3ABSitzmann_(WMF)/MCS/Test/Title_with_%3A"/>
         </head>
         <body>
-            <a rel="mw:WikiLink" href="./User:BSitzmann_(WMF)/MCS/Test/Title_with_:#Section_1" 
+            <a rel="mw:WikiLink" href="./User:BSitzmann_(WMF)/MCS/Test/Title_with_:#Section_1"
                id="mwDw">#Section 1</a>
         </body>
        </html>`;
 
-describe('lib:parsoid-access', function() {
-
-    this.timeout(20000); // eslint-disable-line no-invalid-this
+describe('lib:parsoid-access', () => {
 
     it('getBaseUri()', () => {
         const doc = domino.createDocument(headHtml1);
-        assert.equal(parsoid._getBaseUri(doc), '//en.wikipedia.org/wiki/');
+        assert.deepEqual(parsoid.getBaseUri(doc), '//en.wikipedia.org/wiki/');
     });
 
     it('getParsoidLinkTitle should return DB title', () => {
         const doc = domino.createDocument(headHtml1);
-        assert.equal(parsoid._getParsoidLinkTitle(doc), 'Hope_(painting)');
+        assert.deepEqual(parsoid.getParsoidLinkTitle(doc), 'Hope_(painting)');
     });
 
     it('getParsoidLinkTitle should percent-decode title', () => {
         const doc = domino.createDocument(headHtml2);
-        assert.equal(parsoid._getParsoidLinkTitle(doc),
+        assert.deepEqual(parsoid.getParsoidLinkTitle(doc),
             'User:BSitzmann_(WMF)/MCS/Test/Title_with_:');
     });
+
+});
+
+describe('lib:parsoid-access etag handling', () => {
+
+    it('gets strong etag with no quotes', () => {
+        const headers = { etag: "123/Foo" };
+        assert.deepEqual(parsoid.getEtagFromHeaders(headers), '123/Foo');
+    });
+
+    it('strips prefix from weak etags', () => {
+        const headers = { etag: 'W/"123/Foo"' };
+        assert.deepEqual(parsoid.getEtagFromHeaders(headers), '123/Foo');
+    });
+
+    it('gets revision from etag', () => {
+        const headers = { etag: "123/Foo" };
+        assert.deepEqual(parsoid.getRevisionFromEtag(headers), '123');
+    });
+
+    it('gets revision and tid from etag', () => {
+        const headers = { etag: "123/Foo" };
+        const revTid = parsoid.getRevAndTidFromEtag(headers);
+        assert.deepEqual(revTid.revision, '123');
+        assert.deepEqual(revTid.tid, 'Foo');
+    });
+
+    it('getEtagFromHeaders handles undefined input', () => {
+        assert.deepEqual(parsoid.getEtagFromHeaders(), undefined);
+        assert.deepEqual(parsoid.getEtagFromHeaders({}), undefined);
+    });
+
+    it('getRevisionFromEtag handles undefined input', () => {
+        assert.deepEqual(parsoid.getRevisionFromEtag(), undefined);
+        assert.deepEqual(parsoid.getRevisionFromEtag({}), undefined);
+    });
+
+    it('getRevAndTidFromEtag handles undefined input', () => {
+        assert.deepEqual(parsoid.getRevAndTidFromEtag(), undefined);
+        assert.deepEqual(parsoid.getRevAndTidFromEtag({}), undefined);
+    });
+
 });
