ed13896c9997d86ada8840a3b9540025646c8e40
BearND <bsitzmann@wikimedia.org>
2018-03-27 00:34:14 -0600

    Add ReadingList survey announcement
    This is for the Android beta app only right now. The newly released
    version also responds to a new platform string, AndroidAppV2.
    The new Android version also comes with some new conditional parameters
    for filtering it can understand:
    * beta - optional boolean field for filtering on whether or not the app
      is pre-production.
    * logged_in - optional boolean field for filtering on whether or not the
      user is logged in.
    * reading_list_sync_enabled - optional boolean field for filtering on
      whether or not the user has enabled reading list sync.
    * min_version - optional string specifying the minimum version code for
      which to show the announcement.
    * max_version - optional string specifying the maximum version code for
      which to show the announcement.
    
    This is for enwiki and English speaking countries only at this time.
    Start: 04/03/2018 15:00Z (next Tuesday morning US time)
    End:   04/17/2018 15:00Z (two weeks later)
    
    Added negative_text so the Android app shows the dismiss button.
    Added image_height so the Android app can use that to adjust the height
    of the image.
    Added unit test to make sure the countries field is an array of strings.
    
    Bug: T188431
    Change-Id: I61ecf2c97534c43fed4090632a72dbe5cf9366a4

--- etc/feed/announcements.js
+++ etc/feed/announcements.js
@@ -5,85 +5,46 @@ const AnnouncementType = {
     FUNDRAISING: 'fundraising'
 };
 
-const type = AnnouncementType.FUNDRAISING;
+const type = AnnouncementType.SURVEY;
 const activeWiki = 'en.wikipedia.org';
-const startTime = '2017-11-30T16:00:00Z';
-const endTime = '2017-12-20T23:59:00Z';
-const idPrefix = 'EN1217';
+const startTime = '2018-04-03T15:00:00Z';
+const endTime = '2018-04-17T15:00:00Z';
+const androidMinVersion = 230;
+const androidMaxVersion = 240;
+const idPrefix = 'EN0418';
 const androidDestinationUrl
-    = 'https://donate.wikimedia.org/?uselang=en&utm_medium=WikipediaAppFeed&utm_campaign=Android&utm_source=app_201712_6C_control';
-const iosDestinationUrl
-    = 'https://donate.wikimedia.org/?uselang=en&utm_medium=WikipediaAppFeed&utm_campaign=iOS&utm_source=app_201712_6C_control';
-// TODO: consider removing jshint
-// const imageUrl = undefined; // no image this time
-const buttonLabel = 'Donate now';
+    = 'https://docs.google.com/forms/d/e/1FAIpQLSdbDr9g5jqWMenW5TQH__H-WGz3iYZC9T3ASeMzCgUFh7foDQ/viewform';
+const imageUrl = 'https://upload.wikimedia.org/wikipedia/commons/f/f6/Announcement_card_-_sync_reading_list_feedback_form.png';
+const imageHeight = 80; // usually 160dp for Android
+const buttonLabel = 'Send feedback';
+const negativeButtonLabel = 'No thanks';
 const disclaimerHtml
-    = 'By submitting, you are agreeing to our <a href="https://wikimediafoundation.org/wiki/Donor_policy/en">donor privacy policy</a>.';
+    = 'Feedback form powered by 3rd-party service; see <a href="https://wikimediafoundation.org/wiki/Reading_List_Sync_Feedback_Survey_Privacy_Statement">privacy statement</a>.';
 
-const countryVariants = [ {
-    countryCode: 'US',
-    country: 'the U.S.',
-    currency: '$',
-    average: 15,
-    coffee: 3
-}, {
-    countryCode: 'GB',
-    country: 'the UK',
-    currency: '£',
-    average: 10,
-    coffee: 2
-}, {
-    countryCode: 'AU',
-    country: 'Australia',
-    currency: '$',
-    average: 15,
-    coffee: 3
-}, {
-    countryCode: 'CA',
-    country: 'Canada',
-    currency: '$',
-    average: 15,
-    coffee: 3
-}, {
-    countryCode: 'NZ',
-    country: 'New Zealand',
-    currency: '$',
-    average: 15,
-    coffee: 3
-}, {
-    countryCode: 'IE',
-    country: 'Ireland',
-    currency: '€',
-    average: 10,
-    coffee: 2
-}];
+const countries = [ 'US', 'CA', 'GB', 'IE', 'AU', 'NZ' ];
 
 /**
- * Builds the body text. No HTML here since iOS doesn't support it.
- * @param {!object} vars an Object holding the variables for text substitution
+ * Builds the body text for Android. HTML is ok here since it's not for iOS.
  */
-const buildBaseBodyText = ({ country, currency, average, coffee }) => {
-    // replaced %COUNTRY% with ${country}, ...
-    /* eslint-disable max-len */
-    return `Hi reader in ${country}, it seems you use Wikipedia a lot; I think that’s great and hope you find it useful.
-
-It’s a little awkward to ask, but today we need your help. We depend on donations averaging ${currency}${average}, but fewer than 1% of readers choose to give. If you donate just ${currency}${coffee}, you would help keep Wikipedia thriving for years. That’s right, the price of a cup of coffee is all I ask. Please take a minute to keep Wikipedia growing.
-
-Thank you. — Jimmy Wales, Wikipedia Founder`;
-    /* eslint-enable max-len */
+const buildAndroidBodyText = () => {
+    /* eslint-disable-next-line max-len */
+    return `<b>Hi Android reader</b>, what’s been your experience with using our new reading list syncing feature? Send your feedback in the form linked below.`;
 };
 
 module.exports = {
     activeWiki,
     startTime,
     endTime,
+    androidMinVersion,
+    androidMaxVersion,
     idPrefix,
     type,
     androidDestinationUrl,
-    iosDestinationUrl,
-    // imageUrl,
+    imageUrl,
+    imageHeight,
     buttonLabel,
+    negativeButtonLabel,
     disclaimerHtml,
-    countryVariants,
-    buildBaseBodyText
+    countries,
+    buildAndroidBodyText
 };
