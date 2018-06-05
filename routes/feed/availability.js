fdb1567b518e5c938ec03cb5efa1fb3d8edb8095
Michael Holloway <mholloway@wikimedia.org>
2018-06-05 09:59:01 -0400

    Availability: Provide full project domains for supported projects
    This future-proofs the response against the possibility of feed content
    being provided for other projects in addition to Wikipedia in the future,
    and improves the consistency with the rest of the REST API.
    
    Bug: T191785
    Change-Id: I39b65ea335470d8315a6de8883988cebd5f9a836

--- routes/feed/availability.js
+++ routes/feed/availability.js
@@ -2,19 +2,23 @@
 
 const mUtil = require('../../lib/mobile-util');
 const sUtil = require('../../lib/util');
-const tfaLangs = require('../../lib/feed/featured').supportedLangs;
+const tfaDomains = require('../../lib/feed/featured').supportedDomains;
 const newsLangs = Object.keys(require('../../etc/feed/news-sites'));
 const onThisDayLangs = Object.keys(require('../../lib/feed/on-this-day.languages').languages);
 
 const router = sUtil.router();
 
+function langCodesToWikipediaDomains(langCodes) {
+    return langCodes.map(lang => `${lang}.wikipedia.org`);
+}
+
 router.get('/availability', (req, res) => {
     const response = {
-        todays_featured_article: tfaLangs,
+        todays_featured_article: tfaDomains,
         most_read: ['*'],
         picture_of_the_day: ['*'],
-        in_the_news: newsLangs,
-        on_this_day: onThisDayLangs
+        in_the_news: langCodesToWikipediaDomains(newsLangs),
+        on_this_day: langCodesToWikipediaDomains(onThisDayLangs)
     };
     res.status(200);
     mUtil.setETag(res,  mUtil.hashCode(JSON.stringify(response)));
