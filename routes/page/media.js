ac0c60840ea712a11ed7c4bd55f81a4630c6f0d3
Michael Holloway <mholloway@wikimedia.org>
2018-07-06 14:32:55 -0400

    Media: Call RESTBase and MW API in parallel to improve performance
    Makes the two major API calls in parallel rather than in series in an
    effort to improve the performance of this endpoint.
    
    This requires a couple of noteworthy changes:
    
    * A mwApiQuery method is added to api-util.  This is because the MW API
      call for image info, which relies on `generator=images`, requires
      continuation for large pages (containing 50+ unique images). The new
      method handles query continuation.
    
    * Calling the MW API initially removes our ability to specify a revision,
      so the spec is updated to no longer advertise support for specific
      revisions or tids.  This should not be a problem for clients in
      practice.  The minor version for the media endpoint is bumped to reflect
      the change.  The diff result fixture is also updated accordingly.
    
    Note: When testing locally with ab I found that this performs better for
    most pages, but slightly worse for pages requiring query continuation.
    It's probably worth trying out to see the net effect in production (and
    note that the penalty for query continuation may be less there, since
    we'll be making internal API calls rather than hitting public endpoints).
    
    Bug: T189830
    Change-Id: Ia8ba690943647ba6be81c05f9a646bd47d33d850

--- routes/page/media.js
+++ routes/page/media.js
@@ -15,30 +15,22 @@ let app;
  * Gets the media items associated with the given page.
  */
 router.get('/media/:title/:revision?/:tid?', (req, res) => {
-    return BBPromise.join(
+    return mwapi.getSiteInfo(app, req)
+    .then(si => BBPromise.join(
         parsoid.getParsoidHtml(app, req),
-        mwapi.getSiteInfo(app, req),
-        (html, siteinfo) => {
+        lib.getImageInfo(app, req, si),
+        (html, imageInfo) => {
             const revTid = parsoid.getRevAndTidFromEtag(html.headers);
             const pageMediaList = lib.getMediaItemInfoFromPage(html.body);
-            if (!pageMediaList.length) {
-                res.send({ items: [] });
-                return;
-            }
-            const titles = mUtil.deduplicate(pageMediaList.filter(i => i.title).map(i => i.title));
-            return lib.getMetadataFromApi(app, req, titles, siteinfo)
-            .then((apiResponse) => {
-                const result = lib.combineResponses(apiResponse, pageMediaList);
-                mUtil.setETag(res, revTid.revision, revTid.tid);
-                mUtil.setContentType(res, mUtil.CONTENT_TYPES.media);
-                mUtil.setLanguageHeaders(res, html.headers);
-                res.send({
-                    revision: revTid.revision,
-                    tid: revTid.tid,
-                    items: result
-                });
+            mUtil.setETag(res, revTid.revision, revTid.tid);
+            mUtil.setContentType(res, mUtil.CONTENT_TYPES.media);
+            mUtil.setLanguageHeaders(res, html.headers);
+            res.send({
+                revision: revTid.revision,
+                tid: revTid.tid,
+                items: lib.combineResults(imageInfo, pageMediaList)
             });
-        });
+        }));
 });
 
 module.exports = function(appObj) {
