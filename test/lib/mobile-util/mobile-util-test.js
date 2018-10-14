c2a4ef9fdb1d5731bc93820efce379ba4a538e55
Michael Holloway <mholloway@wikimedia.org>
2018-10-14 11:37:49 -0400

    Strip 'accept' value from vary header pass-through
    Don't include the 'accept' value in the response vary header since the
    response doesn't actually vary based on the request accept header.
    
    Bug: T206701
    Change-Id: Id636cb525723a8c5454c314b2c02c22076d59e53

--- test/lib/mobile-util/mobile-util-test.js
+++ test/lib/mobile-util/mobile-util-test.js
@@ -3,6 +3,7 @@
 const assert = require('../../utils/assert');
 const mUtil = require('../../../lib/mobile-util');
 const domino = require('domino');
+const MockResponse = require('mock-express-response');
 
 describe('lib:mobile-util', () => {
 
@@ -43,5 +44,60 @@ describe('lib:mobile-util', () => {
         // eslint-disable-next-line max-len
         assert.deepEqual(mUtil.domainForLangCode('de.wikipedia.beta.wmflabs.org', 'ja'), 'ja.wikipedia.beta.wmflabs.org');
         assert.deepEqual(mUtil.domainForLangCode('mediawiki.org', 'es'), 'mediawiki.org');
+    });
+
+    describe('setLanguageHeaders', () => {
+
+        it('passes through headers (lower-case names in original)', () => {
+            const res = new MockResponse();
+            mUtil.setLanguageHeaders(res, {
+                vary: 'foo',
+                'content-language': 'bar'
+            });
+            assert.deepEqual(res.get('Vary'), 'foo');
+            assert.deepEqual(res.get('Content-Language'), 'bar');
+        });
+
+        it('passes through headers (upper-case names in original)', () => {
+            const res = new MockResponse();
+            mUtil.setLanguageHeaders(res, {
+                Vary: 'foo',
+                'Content-Language': 'bar'
+            });
+            assert.deepEqual(res.get('Vary'), 'foo');
+            assert.deepEqual(res.get('Content-Language'), 'bar');
+        });
+
+        it('strips \'accept\' from vary value with other values present', () => {
+            const res = new MockResponse();
+            mUtil.setLanguageHeaders(res, {
+                vary: 'accept, accept-language',
+            });
+            assert.deepEqual(res.get('Vary'), 'accept-language');
+        });
+
+        it('strips \'Accept\' from vary value with other values present', () => {
+            const res = new MockResponse();
+            mUtil.setLanguageHeaders(res, {
+                Vary: 'Accept, Accept-Language',
+            });
+            assert.deepEqual(res.get('Vary'), 'Accept-Language');
+        });
+
+        it('strips vary header if set to \'Accept\' only', () => {
+            const res = new MockResponse();
+            mUtil.setLanguageHeaders(res, {
+                Vary: 'Accept',
+            });
+            assert.deepEqual(res.get('Vary'), undefined);
+        });
+
+        it('strips vary header if set to \'accept\' only', () => {
+            const res = new MockResponse();
+            mUtil.setLanguageHeaders(res, {
+                Vary: 'accept',
+            });
+            assert.deepEqual(res.get('Vary'), undefined);
+        });
     });
 });
