58cbdff6031157c6aeeb073d54a6dde7ae5f4169
BearND <bsitzmann@wikimedia.org>
2018-10-15 13:57:41 -0600

    mobile-sections: move style tags in ref lists to end
    References on enwiki are using template styles, which breaks parsing of
    references in the Android app. There is a page library PR to fix that
    but that wouldn't cover older installations.
    
    This patch moves all <style> elements inside reference lists to the end
    of the document, while avoid duplicate styles. The deduplication is not
    exhaustive, though, since this would miss any styles outside of ref
    lists already before this transformation but it should get us 95% there
    (TM).
    
    This is taking advantage of the data-mw-deduplicate attributes generated
    by TemplateStyles. If there are other style elements present not having
    these they will be simply removed.
    
    Bumped mobile-sections patch version to 0.14.4.
    
    Bug: T206527
    Change-Id: I9a6a6c173141ec2c4379f5bd8e3f1fb2451b8616

---
+++ test/lib/references/moveReferenceListStyles-unit.js
@@ -0,0 +1,86 @@
+'use strict';
+
+const assert = require('../../utils/assert.js');
+const domino = require('domino');
+const lib = require('../../../lib/references/moveReferenceListStyles');
+const moveReferenceListStyles = lib.moveReferenceListStyles;
+
+describe('lib:moveReferenceListStyles', () => {
+    it('empty document', () => {
+        const doc = domino.createDocument('');
+
+        moveReferenceListStyles(doc);
+        assert.selectorExistsNTimes(doc, 'ol.mw-references style', 0);
+    });
+
+    it('one list, one template style', () => {
+        const doc = domino.createDocument(
+            '<ol typeof="mw:Extension/references" class="mw-references references">'
+            + '<li><span>foo A1</span><cite>foo</cite>'
+            + '<style data-mw-deduplicate="123"></style>'
+            + '</li>'
+            + '</ol>');
+
+        assert.selectorExistsNTimes(doc, 'ol.mw-references style', 1);
+        moveReferenceListStyles(doc);
+        assert.selectorExistsNTimes(doc, 'ol.mw-references style', 0);
+        assert.selectorExistsNTimes(doc, 'style', 1);
+    });
+
+    it('style outside ref list stays', () => {
+        const doc = domino.createDocument(
+            '<style data-mw-deduplicate="123"></style>'
+            + '<ol typeof="mw:Extension/references" class="mw-references references">'
+            + '<li><span>foo A1</span><cite>foo</cite></li>'
+            + '</ol>');
+
+        assert.selectorExistsNTimes(doc, 'ol.mw-references style', 0);
+        assert.selectorExistsNTimes(doc, 'style', 1);
+        moveReferenceListStyles(doc);
+        assert.selectorExistsNTimes(doc, 'ol.mw-references style', 0);
+        assert.selectorExistsNTimes(doc, 'style', 1);
+    });
+
+    it('one list, two template styles; +basic deduplication', () => {
+        const doc = domino.createDocument(
+            '<ol typeof="mw:Extension/references" class="mw-references references">'
+            + '<li><span>foo A1</span><cite>foo</cite>'
+            + '<style data-mw-deduplicate="123"></style>'
+            + '</li>'
+            + '<li><span>foo A2</span><cite>foo</cite>'
+            + '<style data-mw-deduplicate="123"></style>'
+            + '</li>'
+            + '</ol>');
+
+        assert.selectorExistsNTimes(doc, 'ol.mw-references style', 2);
+        moveReferenceListStyles(doc);
+        assert.selectorExistsNTimes(doc, 'ol.mw-references style', 0);
+        assert.selectorExistsNTimes(doc, 'style', 1);
+    });
+
+    it('two lists, two template styles; +basic deduplication', () => {
+        const doc = domino.createDocument(
+            '<ol typeof="mw:Extension/references" class="mw-references references">'
+            + '<li><span>foo A1</span><cite>foo</cite>'
+            + '<style data-mw-deduplicate="123"></style>'
+            + '</li>'
+            + '<li><span>foo A2</span><cite>foo</cite>'
+            + '<style data-mw-deduplicate="123"></style>'
+            + '</li>'
+            + '</ol>'
+            + '<ol typeof="mw:Extension/references" class="mw-references references">'
+            + '<li><span>foo B1</span><cite>foo</cite>'
+            + '<style data-mw-deduplicate="123"></style>'
+            + '</li>'
+            + '<li><span>foo B2</span><cite>foo</cite>'
+            + '<style data-mw-deduplicate="123"></style>'
+            + '</li>'
+            + '</ol>'
+        );
+
+        assert.selectorExistsNTimes(doc, 'ol.mw-references style', 4);
+        moveReferenceListStyles(doc);
+        assert.selectorExistsNTimes(doc, 'ol.mw-references style', 0);
+        assert.selectorExistsNTimes(doc, 'style', 1);
+    });
+});
