c80aed8b63438a8ca4a927caae9aa8c76f75d99e
Michael Holloway <mholloway@wikimedia.org>
2018-11-14 14:23:05 -0600

    Definition: Streamline Parsoid HTML preprocessing
    Removes a substantial number of preprocessing steps to improve the
    endpoint's performance. This results in a slightly (but acceptably)
    larger definition response.
    
    Also removes a test made redundant by the recently added definition diff
    test.
    
    Change-Id: I0c8337c2c0661d3df70afef2ca5d5cf6822bfefc

--- test/features/definition/definition.js
+++ test/features/definition/definition.js
@@ -10,50 +10,6 @@ describe('definition', function() {
 
     before(() => server.start());
 
-    it("en 'cat' request should have expected structure and content", () => {
-        return preq.get(
-            { uri: `${server.config.uri}en.wiktionary.org/v1/page/definition/cat/42803194` })
-            .then((res) => {
-                const en = res.body.en;
-                const bodytext = JSON.stringify(res.body);
-                assert.ok(bodytext.indexOf('ib-brac') === -1);
-                assert.ok(bodytext.indexOf('ib-content') === -1);
-                assert.ok(bodytext.indexOf('defdate') === -1);
-                assert.deepEqual(res.status, 200);
-                assert.notDeepEqual(en, undefined);
-                assert.equal(en.length, 8, JSON.stringify(en, null, 2));
-                for (let i = 0; i < en.length; i++) {
-                    assert.notDeepEqual(en[i].partOfSpeech, undefined);
-                    assert.notDeepEqual(en[i].definitions, undefined);
-                    for (let j = 0; j < en[i].definitions.length; j++) {
-                        assert.notDeepEqual(en[i].definitions[j].definition, undefined);
-                        const examples = en[i].definitions[j].examples;
-                        if (examples) {
-                            assert.ok(examples.length > 0);
-                            for (const example of examples) {
-                                assert.ok(example.trim().length > 0);
-                            }
-                        }
-                        const parsedExamples = en[i].definitions[j].parsedExamples;
-                        if (parsedExamples) {
-                            assert.ok(parsedExamples.length > 0);
-                            for (const parsedExample of parsedExamples) {
-                                assert.ok(parsedExample.example.trim().length > 0);
-                            }
-                        }
-                    }
-                }
-                assert.deepEqual(en[0].partOfSpeech, 'Noun');
-                const def0 = en[0].definitions[0].definition;
-                assert.ok(def0.indexOf('An animal of the family ') === 0,
-                    'Expected different start of definition specifying family');
-                assert.deepEqual(en[1].partOfSpeech, 'Verb');
-                const def1 = en[1].definitions[0].definition;
-                assert.ok(def1.indexOf('To <a href="/wiki/hoist" title="hoist"') === 0,
-                    'Expected different start of definition linking to hoist');
-            });
-    });
-
     it('missing definitions', () => {
         const uri = `${server.config.uri}en.wiktionary.org/v1/page/definition/Dssjbkrt`;
         return preq.get({ uri })
