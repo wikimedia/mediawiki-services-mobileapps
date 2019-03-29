a8342eefb0b36fa0e2e2fb38c03dbc5121ae566e
Michael Holloway <mholloway@wikimedia.org>
2019-03-29 18:32:31 -0400

    Add Commons Wikibase entity IDs to media response
    Adds the Commons Wikibase entity ID for files that exist on Commons,
    i.e., freely-licensed images.
    
    Note that wbgetentities support does not seem to exist yet for most
    files.
    
    Bug: T219667
    Change-Id: I751c6987c219efae4f9fbab4310c3153dcddb3da

--- test/features/media/pagecontent.js
+++ test/features/media/pagecontent.js
@@ -24,12 +24,16 @@ describe('media', function() {
                 assert.ok(res.body.items.length > 0, 'Expected at least one media item');
             });
     });
-    it('en Barack Obama should have many media items', () => {
+    it('en Barack Obama should have many media items with valid entity IDs', () => {
         return preq.get({ uri: `${server.config.uri}en.wikipedia.org/v1/page/media/Barack_Obama` })
             .then((res) => {
                 const items = res.body.items;
                 assert.deepEqual(res.status, 200);
                 assert.ok(items.length > 3, 'Expected many media items');
+                assert.ok(items.filter(i => i.wb_entity_id).length > 3,
+                    'Expected many media items to have Commons Wikibase entity IDs');
+                assert.ok(items.filter(i => i.wb_entity_id === 'Mundefined').length === 0,
+                    'Expected no items to have invalid entity ID "Mundefined"');
             });
     });
     it('Missing title should respond with 404', () => {
