cdb9928cb1b63d14a6c0b71c983a807b06c2ab8b
Michael Holloway <mholloway@wikimedia.org>
2019-04-02 10:11:06 -0400

    Bifurcate imageinfo queries to improve performance
    When the Action API is queried on a non-Commons wiki (e.g., a Wikipedia)
    for imageinfo, the API code first searches for the requested data in the
    wiki's local file repository, then falls back to the Commons file
    repository (where most files in use on the wikis are actually located).
    
    This patch improves the performance of the /page/media endpoint by using
    the upload.wikimedia.org URL paths to identify whether the file is
    located in Commons or the local wiki's file repository, then directing
    imageinfo requests directly to the appropriate wiki.
    
    Based on local performance testing, this consistently saves about 100 ms
    on the average request.
    
    Note: This also adds a new "repository" field to results, containing as a
    value either "commons" or "local".
    
    Change-Id: I7ff6241bfa110aad113a33dc4caeb5342ad67604

--- routes/page/media.js
+++ routes/page/media.js
@@ -11,6 +11,10 @@ const imageinfo = require('../../lib/imageinfo');
 const router = sUtil.router();
 let app;
 
+function getDeduplicatedTitles(list) {
+    return mUtil.deduplicate(list.filter(i => i.title).map(i => i.title));
+}
+
 /**
  * GET {domain}/v1/page/media/{title}{/revision}{/tid}
  * Gets the media items associated with the given page.
@@ -22,12 +26,14 @@ router.get('/media/:title/:revision?/:tid?', (req, res) => {
         (html, siteinfo) => {
             const revTid = parsoid.getRevAndTidFromEtag(html.headers);
             const pageMediaList = lib.getMediaItemInfoFromPage(html.body);
-            if (!pageMediaList.length) {
+            if (!pageMediaList.length && !pageMediaList.length) {
                 res.send({ items: [] });
                 return;
             }
-            const titles = mUtil.deduplicate(pageMediaList.filter(i => i.title).map(i => i.title));
-            return imageinfo.getMetadataFromApi(app, req, titles, siteinfo)
+            return imageinfo.getMetadataFromApi(app, req,
+                getDeduplicatedTitles(pageMediaList.filter(i => i.repository === 'commons')),
+                getDeduplicatedTitles(pageMediaList.filter(i => i.repository === 'local')),
+                siteinfo)
             .then((apiResponse) => {
                 const result = lib.combineResponses(apiResponse, pageMediaList);
                 mUtil.setETag(res, revTid.revision, revTid.tid);
