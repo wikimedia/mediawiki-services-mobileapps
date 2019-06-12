b9f01d84d0b775921a39d240508306bbd10dc9ec
Michael Holloway <mholloway@wikimedia.org>
2019-06-12 16:13:10 -0400

    Talk: Call setLanguageHeaders on the response object
    As a follow-up to d543a0f, sets Content-Language and/or Vary headers
    on the response as needed.
    
    Hygiene: Makes routes/page/talk.js more concise and changes margins
    there to the repo-standard four spaces.
    
    Change-Id: I9c07f321d8e7ea215d4955d0119fec9b2849acae

--- routes/page/talk.js
+++ routes/page/talk.js
@@ -8,42 +8,24 @@ const parsoidApi = require('../../lib/parsoid-access');
  */
 let app;
 
-const fetchParsoidHtmlForTitle = (app, req, title) => {
-  const parsoidReq = Object.create(req);
-  parsoidReq.params.title = title;
-  return parsoidApi.getParsoidHtml(app, parsoidReq);
-};
-
-const fetchDocAndRevision = (app, req) => {
-  let revision;
-  return fetchParsoidHtmlForTitle(app, req, req.params.title)
-  .then((response) => {
-      revision = parsoidApi.getRevisionFromEtag(response.headers);
-      return response.body;
-  })
-  .then(mUtil.createDocument)
-  .then(doc => [doc, revision]);
-};
-
 /**
  * GET {domain}/v1/page/talk/{title}{/revision}{/tid}
  * Gets talk page info.
  */
 router.get('/talk/:title/:revision?/:tid?', (req, res) => {
-  return fetchDocAndRevision(app, req)
-    .then(docAndRevision => {
-      const doc = docAndRevision[0];
-      const revision = docAndRevision[1];
-      const lang = req.params.domain.split('.')[0];
+    return parsoidApi.getParsoidHtml(app, req)
+    .then(parsoidRsp => mUtil.createDocument(parsoidRsp.body)
+    .then((doc) => {
+        const lang = req.params.domain.split('.')[0];
+        const topicsWithReplies = talkParser.parseUserTalkPageDocIntoTopicsWithReplies(doc, lang);
 
-      const topicsWithReplies = talkParser.parseUserTalkPageDocIntoTopicsWithReplies(doc, lang);
-
-      res.status(200);
-      mUtil.setETag(res, revision);
-      mUtil.setContentType(res, mUtil.CONTENT_TYPES.talk);
-      res.json(topicsWithReplies).end();
-    });
-  });
+        res.status(200);
+        mUtil.setETag(res, parsoidApi.getRevAndTidFromEtag(parsoidRsp.headers));
+        mUtil.setContentType(res, mUtil.CONTENT_TYPES.talk);
+        mUtil.setLanguageHeaders(res, parsoidRsp.headers);
+        res.json(topicsWithReplies).end();
+    }));
+});
 
 module.exports = function(appObj) {
     app = appObj;
