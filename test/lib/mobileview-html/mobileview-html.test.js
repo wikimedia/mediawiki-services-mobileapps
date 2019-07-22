f8e220e1e54c89ecc044a868cda0708f4bfb14c6
BearND <bsitzmann@wikimedia.org>
2019-07-22 13:36:15 -0600

    mobile-html: use mobileview for zhwiki
    Chinese language variants are not supported by Parsoid yet.
    So fallback to action=mobileview and stitch the page together
    server-side.
    We're trying to get the low-hanging fruit portions of making it look
    like Parsoid output but assume this is only temporary until Parsoid
    does support Chinese language variants.
    
    To make the regular processing script for mobile-html work this also:
    * Creates the section headings like Parsoid
    * Rewrites links starting with `/wiki/` to `./`
    * Wraps images inside a `<figure>` tag to make image widening work.
    
    `addPageHeader()` is called directly instead of the post processing
    script since we already have the MW API response.
    
    Bug: T223400
    Change-Id: I324baf14a17ae315657b4b585b7f97ed3999536e

---
+++ test/lib/mobileview-html/mobileview-html.test.js
@@ -0,0 +1,49 @@
+'use strict';
+
+const domino = require('domino');
+const assert = require('../../utils/assert');
+const lib = require('../../../lib/mobileview-html');
+
+describe('lib:mobileview-html', () => {
+    describe('buildSection', () => {
+        it('section 0', () => {
+            const document = domino.createDocument('');
+            const result = lib.testing.buildSection(document,
+                { id: 0, text: 'lead' });
+            assert.deepEqual(result.outerHTML,
+                '<section data-mw-section-id="0" id="content_block_0">lead</section>');
+        });
+        it('section 1', () => {
+            const document = domino.createDocument('');
+            const result = lib.testing.buildSection(document,
+                {
+                    id: 1,
+                    toclevel: 1,
+                    line: 'My section heading',
+                    anchor: 'heading_anchor',
+                    text: '<p>Foo Bar</p>'
+                });
+            assert.deepEqual(result.outerHTML,
+                '<section data-mw-section-id="1"><h2 id="heading_anchor">My section heading</h2><p>Foo Bar</p></section>');
+        });
+    });
+
+    describe('rewriteWikiLinks', () => {
+        it('single link', () => {
+            const document = domino.createDocument('<p><a href="/wiki/A"></a></p>');
+            const result = lib.testing.rewriteWikiLinks(document, 'a', 'href');
+            assert.deepEqual(document.body.innerHTML, '<p><a href="./A"></a></p>');
+        });
+    });
+
+    describe('wrapImagesInFigureElements', () => {
+        it('single image', () => {
+            const document = domino.createDocument(
+                '<p><a class="image" href="target"><img src="foo"/></a></p>');
+            const result = lib.testing.wrapImagesInFigureElements(document,
+                { id: 0, text: 'lead' });
+            assert.deepEqual(document.body.innerHTML,
+                '<p><figure class="mw-default-size"><a class="image" href="target"><img src="foo"></a></figure></p>');
+        });
+    });
+});
