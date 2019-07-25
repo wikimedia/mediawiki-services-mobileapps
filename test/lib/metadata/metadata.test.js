9f3aed89f82cb618a43445471fa62ccc47e4a912
Michael Holloway <mholloway@wikimedia.org>
2019-07-25 13:43:01 -0400

    Fix: Metadata: Bail out of langlinks if titles are empty
    There is a magic word, noexternallanglinks, that empties out the titles
    of langlinks. In the web UI this causes them to redirect to the main
    page.  That possibility wasn't handled here, leading to an error in
    mediawiki-title.
    
    This adds a check and bails out if an empty langlink title is
    encountered.
    
    Bug: T228936
    Change-Id: I8ec2f9341d47b6bd3c2e419be2d8ee7f253dbd06

--- test/lib/metadata/metadata.test.js
+++ test/lib/metadata/metadata.test.js
@@ -1,15 +1,49 @@
 'use strict';
 
+const fs = require('fs');
+const path = require('path');
 const lib = require('../../../lib/metadata').testing;
 const assert = require('../../utils/assert');
 
+const ENWIKI_SITEINFO = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../../fixtures/siteinfo_enwiki.json'), 'utf8'));
+
 describe('lib:metadata', () => {
 
-    it('augmentLangLinks handles undefined langlinks', () => {
-        assert.doesNotThrow(() => lib.augmentLangLinks());
+    describe('augmentLangLinks', () => {
+
+        it('handles undefined langlinks', () => {
+            assert.deepEqual(lib.augmentLangLinks(), undefined);
+        });
+
+        it('bails out if an empty title is found', () => {
+            assert.deepEqual(lib.augmentLangLinks([{ lang: 'test', title: '' }]), undefined);
+        });
+
+        it('bails out if an empty title is found (and nonempty title exists)', () => {
+            assert.deepEqual(lib.augmentLangLinks([
+                { lang: 'test', title: 'Foo' },
+                { lang: 'test2', title: '' }
+            ]), undefined);
+        });
+
+        it('creates augmented langlink if input is good', () => {
+            assert.deepEqual(lib.augmentLangLinks([
+                { lang: 'test', title: 'Foo' },
+            ], 'en.wikipedia.org', ENWIKI_SITEINFO), [
+                {
+                    lang: 'test',
+                    summary_url: 'https://test.wikipedia.org/api/rest_v1/page/summary/Foo',
+                    titles: {
+                        canonical: 'Foo',
+                        normalized: 'Foo'
+                    }
+                }
+            ]);
+        });
+
     });
 
-    it('augmentCategores handles undefined categories', () => {
+    it('augmentCategories handles undefined categories', () => {
         assert.doesNotThrow(() => lib.augmentCategories());
     });
 
