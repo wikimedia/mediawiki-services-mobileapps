085a2efe2dd428d4be2f4ff4d91d6bb0d465c262
Joe Walsh <joewalsh@wikimedia.org>
2019-09-19 12:20:08 -0400

    Utilize new methods in pagelib for body start and body end JS.
    This eliminates the need to keep client-side page JS in mobileapps services and allows the start and end scripts to utilize the page library directly.
    
    Bug: T233253
    Change-Id: I8e369c2161f4e6b6d59936d2b8e021da50edc348

--- routes/data/javascript.js
+++ routes/data/javascript.js
@@ -9,8 +9,6 @@ const router = sUtil.router();
  * Gets the JavaScript from the wikimedia-page-library
  */
 router.get('/pagelib', (req, res) => js.fetchPageLibJs(res));
-router.get('/pagelib_body_start', (req, res) => js.fetchPageLibBodyStartJs(res));
-router.get('/pagelib_body_end', (req, res) => js.fetchPageLibBodyEndJs(res));
 
 module.exports = function(appObj) {
     return {
