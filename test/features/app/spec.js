abd35432581e624a3b3b4634e4175e2c8d608de7
Michael Holloway <mholloway@wikimedia.org>
2019-10-08 09:13:02 -0400

    Hygiene: Stop skipping featured-image spec test
    Change-Id: Id708bf866e75f4907e3636d4847c6bd7a0214e59

--- test/features/app/spec.js
+++ test/features/app/spec.js
@@ -336,8 +336,7 @@ describe('Swagger spec', function() {
             return assertValidSchema(uri, '#/components/schemas/article_summary_merge_link');
         });
 
-        // Skip for 2019-10-07 due to broken POTD template on-wiki
-        it.skip('featured image response should conform to schema', () => {
+        it('featured image response should conform to schema', () => {
             const uri = `${baseUri}media/image/featured/${dateStr1}`;
             return assertValidSchema(uri, '#/components/schemas/image');
         });
