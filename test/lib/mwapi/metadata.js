c90c100332f5800a8b9d67185cf0224c903333f8
Mateus Santos <msantos@wikimedia.org>
2020-01-09 15:40:32 -0300

    mobile-html: always request displaytitle
    1) In order to preserve HTML in page titles, make mobile-html always
    request displaytitle for a given page.
    2) Fix api-util http references
    3) Add to testUtil ability to mock app object and requests
    4) Introduce tests for queryToMetadata
    
    Bug: T242028
    Change-Id: I69d7f153c850a190f4b929ef645e825ce4e05402

---
+++ test/lib/mwapi/metadata.js
@@ -0,0 +1,36 @@
+'use strict';
+
+const assert = require('../../utils/assert.js');
+const queryForMetadata = require('../../../lib/mwapi').queryForMetadata;
+const util = require('../../utils/testUtil');
+
+const responseBuilder = (page, siteinfo, title, description) => {
+    return {
+        page,
+        siteinfo,
+        title,
+        description,
+    };
+};
+
+describe('lib:mwapi:queryForMetadata', () => {
+    it('ensure that displaytitle is always requested', () => {
+        const req = util.getMockedServiceReq({
+            params: { title: 'Homo sapiens', domain: 'en.wikipedia.org' },
+        });
+        const query = {
+            format: 'json',
+            formatversion: 2,
+            action: 'query',
+            prop: 'description|info|pageimages',
+            inprop: 'protection',
+            pilicense: 'any',
+            piprop: 'original',
+            titles: req.params.title,
+        };
+
+        queryForMetadata(req, query, responseBuilder).then(metadata => {
+            assert.deepEqual(metadata.title.display, '<i>Homo sapiens</i>');
+        });
+    });
+});
--- test/utils/testUtil.js
+++ test/utils/testUtil.js
@@ -1,8 +1,14 @@
 'use strict';
 
+const BBPromise = require('bluebird');
 const dateUtil = require('../../lib/dateUtil');
 const Template = require('swagger-router').Template;
+const aUtil    = require('../../lib/api-util');
 const fixtures = require('./fixtures');
+const preq = require('preq');
+const mockReq = require('sinon-express-mock').mockReq;
+const uuidv1 = require('uuid/v1');
+const HTTPError = require('../../lib/util').HTTPError;
 
 const testUtil = {};
 
@@ -31,5 +37,59 @@ testUtil.rbTemplate = new Template({
  * @return {!Document}
  */
 testUtil.readTestFixtureDoc = fixtures.readIntoDocument;
+
+/**
+ * @param {!object} options app configuration options
+ * @return {!object} mocked app object
+ */
+testUtil.getMockedApp = (options = {}) => {
+    const mockedApp = Object.assign({
+        conf: {
+            user_agent: 'WMF Mobile Content Service dev test',
+        }
+    }, options);
+    aUtil.setupApiTemplates(mockedApp);
+    return mockedApp;
+};
+
+/**
+ * @param {!object} options req configuration for mock
+ * @return {!object} mocked service request
+ */
+testUtil.getMockedServiceReq = (options) => {
+    options = Object.assign({
+        headers: {},
+        app: testUtil.getMockedApp(),
+    }, options);
+
+    const req = mockReq(options);
+
+    req.issueRequest = (request) => {
+        if (!(request.constructor === Object)) {
+            request = { uri: request };
+        }
+        if (request.url) {
+            request.uri = request.url;
+            delete request.url;
+        }
+        if (!request.uri) {
+            return BBPromise.reject(new HTTPError({
+                status: 500,
+                type: 'internal_error',
+                title: 'No request to issue',
+                detail: 'No request has been specified'
+            }));
+        }
+        request.method = request.method || 'get';
+        request.headers = request.headers || {};
+        Object.assign(request.headers, {
+            'user-agent': options.app.conf.user_agent,
+            'x-request-id': req.headers['x-request-id'] || uuidv1()
+        });
+        return preq(request);
+    };
+
+    return req;
+};
 
 module.exports = testUtil;
