0e54e46198679f185dd6a28adad49f66ed5e7aa8
Matt Cleinman <mcleinman@wikimedia.org>
2020-04-02 15:11:12 -0700

    Changed to more heavy-handed way of setting size to compensate for WebKit bug in iPadOS 13.
    Bug: T247565
    Change-Id: I653a792a91e16dc2cf4b45f932b17f33a7c5d86c

--- pagelib/src/transform/AdjustTextSize.ts
+++ pagelib/src/transform/AdjustTextSize.ts
@@ -6,10 +6,30 @@
  */
 const setPercentage = (body: HTMLBodyElement, textSize: string): void => {
     if (textSize){
+        // '-webkit-text-size-adjust' is broken on iPadOS 13: https://bugs.webkit.org/show_bug.cgi?id=201404
+        // This hacky code allows us to adjust font sizes, instead of the convenient 2 lines commented
+        // out at the bottom of this function. Once (Apple fixes this bug and) the iPadOS versions
+        // with the bug have become an insignificant amount of our app usage, we can delete the following
+        // few lines and reactivate the two commented-out lines at the end of this function.
+
+        // Notably, as of April 2020, this function is used by iOS but not Android.
+        // Android updates text size w/ `webView.getSettings().setDefaultFontSize(...)`.
+
+        // remove percent sign
+        const requestedTextSizeNumber = Number(textSize.slice(0, -1));
+
+        // Base.css applies a font-size of .9411764706, per discussion w/ Carolyn
+        // we're going to round that to 95% for now.
+        const calculatedStartPercent = .95;
+
+        const calculatedSize = (requestedTextSizeNumber/100) * calculatedStartPercent;
+        const calculatedSizeString = (calculatedSize*100).toString() + "%";
+        (<any>body.style)['font-size'] = calculatedSizeString;
+
         // casting body style to avoid errors with the subscript operator and typescript
         // see https://stackoverflow.com/questions/37655393
-        (<any>body.style)['-webkit-text-size-adjust'] = textSize;
-        (<any>body.style)['text-size-adjust'] = textSize;
+        // (<any>body.style)['-webkit-text-size-adjust'] = textSize;
+        // (<any>body.style)['text-size-adjust'] = textSize;
     }
 }
 
