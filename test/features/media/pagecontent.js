ab26121837f2303f6502a6ac2674e27a6fee3ba3
Yiannis Giannelos <jgiannelos@wikimedia.org>
2022-02-15 18:55:13 +0100

    Use parsoid as a source of truth for media-list
    * Remove media redirection resolution via commons
    * Avoid resolving redirected titles
    * Make sure that `media-list` and `mobile-html` have consistent URLs
    * Make sure that src on thumbnails is the one listed under `media-list`
    * Remove unused reference to functions
    
    Relevant tickets: T230040, T230067
    
    Bug: T288732
    Change-Id: Id6b74bc3024adf7ded26abcad748326c2aec0b7f

--- test/features/media/pagecontent.js
+++ test/features/media/pagecontent.js
@@ -1,22 +1,35 @@
 'use strict';
 
-const preq   = require('preq');
+const preq = require('preq');
+const domino = require('domino');
 const assert = require('../../utils/assert');
 const server = require('../../utils/server');
 
-describe('media', function() {
+describe('media', function () {
 
 	this.timeout(20000);
 
 	before(() => server.start());
-	it('Media-list should resolve redirects for image titles', () => {
-		return preq.get({ uri: `${server.config.uri}en.wikipedia.org/v1/page/media-list/Badminton_at_the_1992_Summer_Olympics` })
-			.then((res) => {
-				const items = res && res.body && res.body.items || [];
-				const resolved = items.filter(item => {
-					return item.title === 'File:Olympic_rings_without_rims.svg';
+	it('Media-list resources should be the same on mobile-html', () => {
+		let mediaListItems;
+		return preq.get({
+			uri: `${server.config.uri}en.wikipedia.org/v1/page/media-list/Badminton_at_the_1992_Summer_Olympics`
+		}).then((res) => {
+			mediaListItems = res && res.body && res.body.items || [];
+			preq.get({
+				uri: `${server.config.uri}en.wikipedia.org/v1/page/mobile-html/Badminton_at_the_1992_Summer_Olympics`
+			}).then((mobileHtmlRes) => {
+				const expectedTitle = 'File:Olympic_rings.svg';
+				const body = domino.createDocument(mobileHtmlRes.body);
+				const mediaListImg = mediaListItems.filter(item => {
+					return item.title === expectedTitle;
 				});
-				assert.ok(resolved.length === 1, 'Expect File:Olympic_rings.svg redirect to be resolved');
+				assert.ok(mediaListImg.length > 0);
+				const expectedURL = '//upload.wikimedia.org/wikipedia/commons/thumb/5/5c/Olympic_rings_without_rims.svg/320px-Olympic_rings_without_rims.svg.png';
+				assert.ok(mediaListImg[0].srcset[0].src === expectedURL);
+				const mobileHtmlImg = body.querySelectorAll(`a[href="./File:${expectedTitle}"]`);
+				assert.ok(mobileHtmlImg.length > 0);
 			});
+		});
 	});
 });
