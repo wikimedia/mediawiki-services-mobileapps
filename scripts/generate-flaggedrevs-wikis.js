fd4fadc10f6ba8204e58d0799b50b2c3813e8697
Vadim Kovalenko <vadim.kovalenko@speedandfunction.com>
2022-06-07 13:26:04 +0300

    Page previews: show flagged revision instead of latest
    Introduce mwapi.getFlaggedOrLatestRevision method to handle wikis with flagged revs in the /summary endpoint
    Add script to parse subdomains of the flagged wikis
    Add flagged json file
    Add unit tests
    
    Bug: T279472
    Change-Id: I3d0778262f5db8d819219abc18b17fe7c6982a33

---
+++ scripts/generate-flaggedrevs-wikis.js
@@ -0,0 +1,57 @@
+'use strict';
+
+const _ = require('underscore');
+const fs = require('fs');
+const preq = require('preq');
+const BBPromise = require('bluebird');
+
+const flaggedWikisUrl = 'https://noc.wikimedia.org/conf/dblists/flaggedrevs.dblist';
+const wikiSitesListUrl = 'https://en.wikipedia.org/w/api.php?action=sitematrix&format=json';
+
+const writeFile = (data) => {
+	const file = fs.createWriteStream(`${__dirname}/../lib/flaggedWikisList.json`,
+		{ flags: 'w' });
+	file.write(JSON.stringify(data, null, 4));
+	file.end();
+};
+
+const fetchUrl = (url) => {
+	return preq.get({ url })
+		.then((response) => {
+			return response;
+		}).catch((err) => {
+			return BBPromise.resolve(`!!! ${err} "${url}" !!!`);
+		});
+};
+
+const convertDbData = (dbnamesData, wikislistData) => {
+	// Remove non-dbname elems (comment line and empty line) from the array
+	const flaggedRevsDbList = dbnamesData.body.toString()
+			.split('\n')
+			.filter(s => !/^\s*#|^\s*$/.test(s)),
+
+		wikislistDataSitematrix = wikislistData.body.sitematrix,
+
+		flattenSites = _.flatten(
+			_.map(Object.entries(wikislistDataSitematrix)
+				.filter(([key, val]) => !isNaN(parseInt(key))), ([key, val]) => val.site)),
+
+		sites = [
+			...flattenSites,
+			...wikislistDataSitematrix.specials
+		];
+
+	return flaggedRevsDbList.map(flaggedWiki => sites.find(siteProp => flaggedWiki === siteProp.dbname).url.replace('https://', ''));
+};
+
+const fetchExtract = (dbnamesUrl, listUrl) => {
+	return BBPromise.join(
+		fetchUrl(dbnamesUrl),
+		fetchUrl(listUrl),
+		(dbnames, wikislist) => {
+			writeFile(convertDbData(dbnames, wikislist));
+		}
+	);
+};
+
+fetchExtract(flaggedWikisUrl, wikiSitesListUrl);
