15c066a73d4a1559a1559a13b51a7fdf32d09567
Yiannis Giannelos <jgiannelos@wikimedia.org>
2025-06-20 11:10:35 +0200

    Bump base images on blubber config to use node 20
    * Remove deprecated service config entry
    * Update bundlesize lib to latest version
    * Rebuild static files to use latest dependencies
    
    Bug: T397313
    Change-Id: I20eeebe0088c154c4cb68fc5480fd45b00002475

--- pagelib/build/wikimedia-page-library-override.js
+++ pagelib/build/wikimedia-page-library-override.js
@@ -1,1 +1,1 @@
-!function(e,t){"object"==typeof exports&&"object"==typeof module?module.exports=t():"function"==typeof define&&define.amd?define([],t):"object"==typeof exports?exports.pcs=t():e.pcs=t()}(this,(()=>(()=>{"use strict";var e={};return e=e.default})()));
+!function(e,t){"object"==typeof exports&&"object"==typeof module?module.exports=t():"function"==typeof define&&define.amd?define([],t):"object"==typeof exports?exports.pcs=t():e.pcs=t()}(this,()=>(()=>{"use strict";var e={};return e=e.default})());
