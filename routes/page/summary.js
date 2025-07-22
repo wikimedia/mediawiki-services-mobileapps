8e336188c076761ab582315f83e0818faa589eae
Cooltey Feng <cfeng@wikimedia.org>
2025-07-22 16:11:24 -0700

    Use mobileview for page/summary extract when needed
     When requesting page/summary endpoint in zhwiki, the text inside the parentheses usually got removed incorrectly, and this issue does not happen on the mobile-html endpoint. This patch does the similar logic, to check the language code to see if we need to either use mobileview or Parsoid.
    
    Bug: T258118
    Change-Id: Idcc3cd28498bbe25866cf758936515a07e94fefd

--- routes/page/summary.js
+++ routes/page/summary.js
@@ -13,6 +13,7 @@ const parsoid = require('../../lib/parsoid-access');
 const sUtil = require('../../lib/util');
 const caching = require('../../lib/caching');
 const { projectAllowMiddlewares } = require('../../lib/wmf-projects');
+const mobileviewHtml = require('../../lib/mobileview-html');
 
 /**
  * The main router object
@@ -60,30 +61,35 @@ router.get('/summary/:title/:revision?/:tid?', summaryMiddlewares, (req, res) =>
 		// Check flagged revision for wikis that have enabled FlaggedRevs extension
 		return BBPromise.resolve(mwapi.getFlaggedOrLatestRevision(req)).then(revision => {
 			req.params.revision = revision;
+			const shouldUseMobileview = mobileviewHtml.shouldUseMobileview(req,
+				app.conf.mobile_view_languages);
+
 			return BBPromise.join(
-				parsoid.getParsoidHtml(req),
+				shouldUseMobileview ? mwapi.getPageFromMobileview(req) :
+					parsoid.getParsoidHtml(req),
 				mwapi.getMetadataForSummary(req, mwapiConstants.LEAD_IMAGE_S),
 				mwapi.getSiteInfo(req),
-				(html, meta, siteinfo) => {
-					const revTid = parsoid.getRevAndTidFromEtag(html.headers);
+				(response, meta, siteinfo) => {
+					const revTid = lib.createRevTid(shouldUseMobileview, response, meta);
+					const htmlContent = lib.extractHtmlContent(shouldUseMobileview, response);
 					return lib.buildSummary(req.params.domain, req.params.title,
-						html.body, revTid, meta, siteinfo, app.conf.processing_scripts.summary)
+						htmlContent, revTid, meta, siteinfo,
+						app.conf.processing_scripts.summary)
 						.then((summary) => {
 							res.status(summary.code);
 							if (summary.code === 200) {
 								delete summary.code;
 								// Don't pass revTid.tid - this response depends on more than
-								// parsoid output. For example, if a wikidata description is edited,
-								// this response will be regenerated, which should trigger a change
-								// in the ETag
+								// parsoid output. For example, if a wikidata description is
+								// edited, this response will be regenerated, which should
+								// trigger a change in the ETag
 
 								// RESTBase compatibility
 								// Required by RESTBase handling of page/summary
 								res.setHeader('content-language', meta.lang);
-
 								mUtil.setETag(res, revTid.revision);
 								mUtil.setContentType(res, mUtil.CONTENT_TYPES.summary);
-								mUtil.setLanguageHeaders(res, html.headers);
+								mUtil.setLanguageHeaders(res, response.headers);
 								res.json(summary);
 							}
 							res.end();
